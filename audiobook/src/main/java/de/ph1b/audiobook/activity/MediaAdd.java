c165b1391663d3e785f6e7a9df3dc42cb547c821
Ph1b <woitaschek@posteo.de>
2014-08-20 09:49:29 +0200

    new folder for samsung

--- audiobook/src/main/java/de/ph1b/audiobook/activity/MediaAdd.java
+++ audiobook/src/main/java/de/ph1b/audiobook/activity/MediaAdd.java
@@ -120,6 +120,7 @@ public class MediaAdd extends ActionBarActivity implements CompoundButton.OnChec
         if (BuildConfig.DEBUG)
             paths.add("/storage/sdcard0/Audiobooks");
         paths.add("/storage/emulated/0");
+        paths.add("/storage/sdcard1");
 
         for (String s : paths)
             if (!dirs.contains(s) && new File(s).isDirectory())
