3aa17659f8fe2d91c530d2d1c6adf651a2d2f42c
Ph1b <woitaschek@posteo.de>
2014-08-20 19:05:44 +0200

    Set white background to prevent transparent background when not started from activity

--- audiobook/src/main/java/de/ph1b/audiobook/fragment/SettingsFragment.java
+++ audiobook/src/main/java/de/ph1b/audiobook/fragment/SettingsFragment.java
@@ -2,6 +2,10 @@ package de.ph1b.audiobook.fragment;
 
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import de.ph1b.audiobook.R;
 
@@ -11,9 +15,15 @@ public class SettingsFragment extends PreferenceFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        if (view != null)
+            view.setBackgroundColor(getResources().getColor(android.R.color.white));
+        return view;
     }
 
 }
