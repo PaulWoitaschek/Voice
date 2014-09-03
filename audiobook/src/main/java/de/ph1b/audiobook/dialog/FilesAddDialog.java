d9b4cf529226ae589c134ad7000986b940dc5d3f
ph1b <woitaschek@posteo.de>
2014-09-03 09:56:08 +0200

    Prepared new dialog for simplification of adding book. At the moment still dead

---
+++ audiobook/src/main/java/de/ph1b/audiobook/dialog/FilesAddDialog.java
@@ -0,0 +1,36 @@
+package de.ph1b.audiobook.dialog;
+
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import de.ph1b.audiobook.R;
+
+public class FilesAddDialog extends DialogFragment{
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        //passing null is fine because of fragment
+        @SuppressLint("InflateParams") View v = inflater.inflate(R.layout.dialog_files_add, null);
+
+        EditText bookTitle = (EditText) v.findViewById(R.id.book_name);
+
+        builder.setView(v);
+
+        builder.setNegativeButton(R.string.choose_time_cancel, null);
+
+        return builder.create();
+    }
+
+}
