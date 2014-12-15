5fab8e824fd3d20efaf4435c233a45cf6f70c8d2
Ph1b <woitaschek@posteo.de>
2014-12-15 01:08:53 +0100

    Only theming on pre lollipop

--- audiobook/src/main/java/de/ph1b/audiobook/utils/MaterialCompatThemer.java
+++ audiobook/src/main/java/de/ph1b/audiobook/utils/MaterialCompatThemer.java
@@ -21,29 +21,33 @@ import de.ph1b.audiobook.R;
 public class MaterialCompatThemer {
 
     public static void theme(SeekBar seekBar) {
-        Resources resources = seekBar.getResources();
+        if (Build.VERSION.SDK_INT < 21) {
+            Resources resources = seekBar.getResources();
 
-        seekBar.getProgressDrawable().setColorFilter(resources.getColor(R.color.colorAccent), PorterDuff.Mode.SRC_ATOP);
-        if (Build.VERSION.SDK_INT >= 16) {
-            seekBar.getThumb().setColorFilter(resources.getColor(R.color.colorAccent), PorterDuff.Mode.SRC_ATOP);
+            seekBar.getProgressDrawable().setColorFilter(resources.getColor(R.color.colorAccent), PorterDuff.Mode.SRC_ATOP);
+            if (Build.VERSION.SDK_INT >= 16) {
+                seekBar.getThumb().setColorFilter(resources.getColor(R.color.colorAccent), PorterDuff.Mode.SRC_ATOP);
+            }
         }
     }
 
     public static void theme(NumberPicker numberPicker) {
-        if (numberPicker != null) {
-            Resources r = numberPicker.getResources();
-            final int count = numberPicker.getChildCount();
-            for (int i = 0; i < count; i++) {
-                View child = numberPicker.getChildAt(i);
-                if (child instanceof EditText) {
-                    try {
-                        Field selectorWheelPaintField = numberPicker.getClass().getDeclaredField("mSelectorWheelPaint");
-                        selectorWheelPaintField.setAccessible(true);
-                        ((Paint) selectorWheelPaintField.get(numberPicker)).setColor(r.getColor(R.color.colorAccent));
-                        ((EditText) child).setTextColor(r.getColor(R.color.colorAccent));
-                        numberPicker.invalidate();
-                    } catch (NoSuchFieldException | IllegalAccessException | IllegalArgumentException e) {
-                        Log.w("setNumberPickerTextColor", e);
+        if (Build.VERSION.SDK_INT < 21) {
+            if (numberPicker != null) {
+                Resources r = numberPicker.getResources();
+                final int count = numberPicker.getChildCount();
+                for (int i = 0; i < count; i++) {
+                    View child = numberPicker.getChildAt(i);
+                    if (child instanceof EditText) {
+                        try {
+                            Field selectorWheelPaintField = numberPicker.getClass().getDeclaredField("mSelectorWheelPaint");
+                            selectorWheelPaintField.setAccessible(true);
+                            ((Paint) selectorWheelPaintField.get(numberPicker)).setColor(r.getColor(R.color.colorAccent));
+                            ((EditText) child).setTextColor(r.getColor(R.color.colorAccent));
+                            numberPicker.invalidate();
+                        } catch (NoSuchFieldException | IllegalAccessException | IllegalArgumentException e) {
+                            Log.w("setNumberPickerTextColor", e);
+                        }
                     }
                 }
             }
@@ -51,18 +55,20 @@ public class MaterialCompatThemer {
     }
 
     public static void theme(Dialog dialog) {
-        if (dialog != null) {
-            Context c = dialog.getContext();
-            Resources resources = dialog.getContext().getResources();
-            int alertTitleId = resources.getIdentifier("alertTitle", "id", "android");
-            TextView alertTitle = (TextView) dialog.getWindow().getDecorView().findViewById(alertTitleId);
-            if (alertTitle != null) {
-                alertTitle.setTextAppearance(c, R.style.TextAppearance_AppCompat_Title);
-            }
-            int titleDividerId = resources.getIdentifier("titleDivider", "id", "android");
-            View titleDivider = dialog.getWindow().getDecorView().findViewById(titleDividerId);
-            if (titleDivider != null) {
-                titleDivider.setBackgroundColor(resources.getColor(android.R.color.transparent)); // change divider color
+        if (Build.VERSION.SDK_INT < 21) {
+            if (dialog != null) {
+                Context c = dialog.getContext();
+                Resources resources = dialog.getContext().getResources();
+                int alertTitleId = resources.getIdentifier("alertTitle", "id", "android");
+                TextView alertTitle = (TextView) dialog.getWindow().getDecorView().findViewById(alertTitleId);
+                if (alertTitle != null) {
+                    alertTitle.setTextAppearance(c, R.style.TextAppearance_AppCompat_Title);
+                }
+                int titleDividerId = resources.getIdentifier("titleDivider", "id", "android");
+                View titleDivider = dialog.getWindow().getDecorView().findViewById(titleDividerId);
+                if (titleDivider != null) {
+                    titleDivider.setBackgroundColor(resources.getColor(android.R.color.transparent)); // change divider color
+                }
             }
         }
     }
