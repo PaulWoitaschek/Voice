428f13662564c615eee227c5ef5c994859e63eb3
Paul Woitaschek <woitaschek@posteo.de>
2015-02-07 09:15:31 +0100

    Using a normal ExecutorService because we dont need the Scheduled functionality

--- audiobook/src/main/java/de/ph1b/audiobook/fragment/BookChooseFragment.java
+++ audiobook/src/main/java/de/ph1b/audiobook/fragment/BookChooseFragment.java
@@ -36,8 +36,8 @@ import com.squareup.picasso.Picasso;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 
 import de.ph1b.audiobook.R;
 import de.ph1b.audiobook.activity.BookPlay;
@@ -205,14 +205,14 @@ public class BookChooseFragment extends Fragment implements View.OnClickListener
         adapter = new MediaAdapter(books, getActivity(), onClickListener);
         recyclerView.setHasFixedSize(true);
 
-        ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+        ExecutorService executor = Executors.newSingleThreadExecutor();
         executor.execute(new Runnable() {
             @Override
             public void run() {
                 db.fillMissingCovers();
-                ArrayList<Book> updatdBooks = db.getAllBooks();
+                ArrayList<Book> updatedBooks = db.getAllBooks();
                 books.clear();
-                books.addAll(updatdBooks);
+                books.addAll(updatedBooks);
                 adapter.notifyDataSetChanged();
             }
         });
