bccd8db55796569c63b3dd5c4b54c153e392bb5e
Paul Woitaschek <woitaschek@posteo.de>
2015-02-07 08:27:04 +0100

    Init widget more efficient by loading the data for the RemoteViews just once and then attaching to all appWidgetIDS at once

--- audiobook/src/main/java/de/ph1b/audiobook/receiver/WidgetProvider.java
+++ audiobook/src/main/java/de/ph1b/audiobook/receiver/WidgetProvider.java
@@ -26,63 +26,73 @@ import de.ph1b.audiobook.utils.Prefs;
 
 public class WidgetProvider extends AppWidgetProvider {
 
+    private void initButtons(RemoteViews remoteViews, Context context) {
+        Intent playPauseI = ServiceController.getPlayPauseIntent(context);
+        PendingIntent playPausePI = PendingIntent.getService(context, KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE, playPauseI, PendingIntent.FLAG_UPDATE_CURRENT);
+        remoteViews.setOnClickPendingIntent(R.id.playPause, playPausePI);
+
+        Intent fastForwardI = ServiceController.getFastForwardIntent(context);
+        PendingIntent fastForwardPI = PendingIntent.getService(context, KeyEvent.KEYCODE_MEDIA_FAST_FORWARD, fastForwardI, PendingIntent.FLAG_UPDATE_CURRENT);
+        remoteViews.setOnClickPendingIntent(R.id.fast_forward, fastForwardPI);
+
+        Intent rewindI = ServiceController.getRewindIntent(context);
+        PendingIntent rewindPI = PendingIntent.getService(context, KeyEvent.KEYCODE_MEDIA_REWIND, rewindI, PendingIntent.FLAG_UPDATE_CURRENT);
+        remoteViews.setOnClickPendingIntent(R.id.rewind, rewindPI);
+
+        if (StateManager.getInstance(context).getState() == PlayerStates.PLAYING) {
+            remoteViews.setImageViewResource(R.id.playPause, R.drawable.ic_pause_white_48dp);
+        } else {
+            remoteViews.setImageViewResource(R.id.playPause, R.drawable.ic_play_arrow_white_48dp);
+        }
+    }
+
+    private void initBookRelated(RemoteViews remoteViews, Context context, int[] appWidgetIds) {
+
+        // get book from database
+        DataBaseHelper db = DataBaseHelper.getInstance(context);
+        Prefs prefs = new Prefs(context);
+        long bookId = prefs.getCurrentBookId();
+        Book book = db.getBook(bookId);
+
+        // if there is no current book, take the first one from all
+        if (book == null) {
+            ArrayList<Book> books = db.getAllBooks();
+            if (books.size() > 0) {
+                book = books.get(0);
+                prefs.setCurrentBookId(book.getId());
+            }
+        }
+
+        // if we have any book, init the views and have a click on the whole widget start BookPlay.
+        // if we have no book, simply have a click on the whole widget start BookChoose.
+        if (book != null) {
+            Picasso.with(context).load(new File(book.getCover())).into(remoteViews, R.id.imageView, appWidgetIds);
+            remoteViews.setTextViewText(R.id.title, book.getName());
+
+            String name = book.getContainingMedia().get(book.getPosition()).getName();
+            remoteViews.setTextViewText(R.id.summary, name);
+
+            Intent wholeWidgetClickI = new Intent(context, BookPlay.class);
+            wholeWidgetClickI.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            PendingIntent wholeWidgetClickPI = PendingIntent.getActivity(context, (int) System.currentTimeMillis(), wholeWidgetClickI, PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setOnClickPendingIntent(R.id.whole_widget, wholeWidgetClickPI);
+        } else {
+            Intent wholeWidgetClickI = new Intent(context, BookChoose.class);
+            wholeWidgetClickI.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            PendingIntent wholeWidgetClickPI = PendingIntent.getActivity(context, (int) System.currentTimeMillis(), wholeWidgetClickI, PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setOnClickPendingIntent(R.id.whole_widget, wholeWidgetClickPI);
+        }
+    }
+
     @Override
     public void onUpdate(final Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
+
+        initButtons(remoteViews, context);
+        initBookRelated(remoteViews, context, appWidgetIds);
+
         for (int appWidgetId : appWidgetIds) {
-            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
-
-            Intent playPauseI = ServiceController.getPlayPauseIntent(context);
-            PendingIntent playPausePI = PendingIntent.getService(context, KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE, playPauseI, PendingIntent.FLAG_UPDATE_CURRENT);
-            remoteViews.setOnClickPendingIntent(R.id.playPause, playPausePI);
-
-            Intent fastForwardI = ServiceController.getFastForwardIntent(context);
-            PendingIntent fastForwardPI = PendingIntent.getService(context, KeyEvent.KEYCODE_MEDIA_FAST_FORWARD, fastForwardI, PendingIntent.FLAG_UPDATE_CURRENT);
-            remoteViews.setOnClickPendingIntent(R.id.fast_forward, fastForwardPI);
-
-            Intent rewindI = ServiceController.getRewindIntent(context);
-            PendingIntent rewindPI = PendingIntent.getService(context, KeyEvent.KEYCODE_MEDIA_REWIND, rewindI, PendingIntent.FLAG_UPDATE_CURRENT);
-            remoteViews.setOnClickPendingIntent(R.id.rewind, rewindPI);
-
-            // get book from database
-            DataBaseHelper db = DataBaseHelper.getInstance(context);
-            Prefs prefs = new Prefs(context);
-            long bookId = prefs.getCurrentBookId();
-            Book book = db.getBook(bookId);
-
-            // if there is no current book, take the first one from all
-            if (book == null) {
-                ArrayList<Book> books = db.getAllBooks();
-                if (books.size() > 0) {
-                    book = books.get(0);
-                    prefs.setCurrentBookId(book.getId());
-                }
-            }
-
-            // if we have any book, init the views and have a click on the whole widget start BookPlay.
-            // if we have no book, simply have a click on the whole widget start BookChoose.
-            if (book != null) {
-                Picasso.with(context).load(new File(book.getCover())).into(remoteViews, R.id.imageView, appWidgetIds);
-                remoteViews.setTextViewText(R.id.title, book.getName());
-
-                if (StateManager.getInstance(context).getState() == PlayerStates.PLAYING) {
-                    remoteViews.setImageViewResource(R.id.playPause, R.drawable.ic_pause_white_48dp);
-                } else {
-                    remoteViews.setImageViewResource(R.id.playPause, R.drawable.ic_play_arrow_white_48dp);
-                }
-
-                String name = book.getContainingMedia().get(book.getPosition()).getName();
-                remoteViews.setTextViewText(R.id.summary, name);
-
-                Intent wholeWidgetClickI = new Intent(context, BookPlay.class);
-                wholeWidgetClickI.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                PendingIntent wholeWidgetClickPI = PendingIntent.getActivity(context, (int) System.currentTimeMillis(), wholeWidgetClickI, PendingIntent.FLAG_UPDATE_CURRENT);
-                remoteViews.setOnClickPendingIntent(R.id.whole_widget, wholeWidgetClickPI);
-            } else {
-                Intent wholeWidgetClickI = new Intent(context, BookChoose.class);
-                wholeWidgetClickI.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                PendingIntent wholeWidgetClickPI = PendingIntent.getActivity(context, (int) System.currentTimeMillis(), wholeWidgetClickI, PendingIntent.FLAG_UPDATE_CURRENT);
-                remoteViews.setOnClickPendingIntent(R.id.whole_widget, wholeWidgetClickPI);
-            }
             appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
         }
     }
