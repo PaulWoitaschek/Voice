80ae23fb80668a7be0c836ecd1f78f347f8790f6
Paul Woitaschek <woitaschek@posteo.de>
2015-04-25 21:09:53 +0200

    Added test for adding and removing thousands of books to find a threading issue that causes recyclerview to have an invalid state

---
+++ audiobook/src/androidTest/java/de/ph1b/audiobook/activity/BookActivityTest.java
@@ -0,0 +1,91 @@
+package de.ph1b.audiobook.activity;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.MediumTest;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.ReentrantLock;
+
+import de.ph1b.audiobook.model.Book;
+import de.ph1b.audiobook.model.Bookmark;
+import de.ph1b.audiobook.model.Chapter;
+import de.ph1b.audiobook.utils.BaseApplication;
+
+
+public class BookActivityTest extends ActivityInstrumentationTestCase2<BookActivity> {
+
+    private final Random rnd = new Random();
+
+    public BookActivityTest() {
+        super(BookActivity.class);
+    }
+
+    @MediumTest
+    public void testAddingRemovingBooksAsynchronous() throws InterruptedException {
+        final BaseApplication baseApplication = (BaseApplication) getActivity().getApplication();
+
+        final ReentrantLock lock = new ReentrantLock();
+        final int THREADS = 1000;
+        final CountDownLatch latch = new CountDownLatch(THREADS);
+
+        for (int i = 0; i < THREADS; i++) {
+            boolean remove = rnd.nextInt(3) > 0; // remove with 1 third prop
+            if (remove) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        lock.lock();
+
+                        for (int i = 0; i < 10; i++) {
+                            ArrayList<Book> allBooks = baseApplication.getAllBooks();
+                            if (allBooks.size() > 0) {
+                                Book book = allBooks.get(rnd.nextInt(allBooks.size()));
+                                baseApplication.deleteBook(book);
+                            }
+                        }
+
+                        lock.unlock();
+                        latch.countDown();
+                    }
+                }).start();
+            } else {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        lock.lock();
+
+                        for (int i = 0; i < 10; i++) {
+                            baseApplication.addBook(randomBook());
+                        }
+
+                        lock.unlock();
+                        latch.countDown();
+                    }
+                }).start();
+            }
+        }
+
+        latch.await();
+
+    }
+
+    private Book randomBook() {
+        ArrayList<Chapter> chapters = new ArrayList<>();
+        chapters.add(new Chapter(randomString(), randomString(), rnd.nextInt()));
+
+        return new Book(randomString(), randomString(), chapters, chapters.get(0).getPath(),
+                Book.Type.COLLECTION_FILE, new ArrayList<Bookmark>(), getActivity());
+
+    }
+
+    private String randomString() {
+        String pool = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String random = "";
+        for (int i = 0; i < 5 + rnd.nextInt(5); i++) {
+            random += pool.charAt(rnd.nextInt(pool.length()));
+        }
+        return random;
+    }
+}
