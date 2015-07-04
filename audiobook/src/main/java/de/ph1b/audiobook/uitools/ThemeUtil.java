c3e3ae98927325b769e5392d2a59a9b1f517acbf
Paul Woitaschek <woitaschek@posteo.de>
2015-07-04 11:48:14 +0200

    Make use of more annotations in theme util

--- audiobook/src/main/java/de/ph1b/audiobook/uitools/ThemeUtil.java
+++ audiobook/src/main/java/de/ph1b/audiobook/uitools/ThemeUtil.java
@@ -8,6 +8,8 @@ import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.preference.PreferenceManager;
+import android.support.annotation.AnyRes;
+import android.support.annotation.AttrRes;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.graphics.drawable.DrawableCompat;
@@ -24,7 +26,7 @@ import de.ph1b.audiobook.R;
 
 public class ThemeUtil {
 
-    public static void theme(SeekBar seekBar) {
+    public static void theme(@NonNull SeekBar seekBar) {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
             int colorAccent = seekBar.getResources().getColor(ThemeUtil.getResourceId(
                     seekBar.getContext(), R.attr.colorAccent));
@@ -49,7 +51,7 @@ public class ThemeUtil {
         }
     }
 
-    public static int getTheme(Context c) {
+    public static int getTheme(@NonNull Context c) {
         Resources r = c.getResources();
 
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(c);
@@ -64,7 +66,8 @@ public class ThemeUtil {
         }
     }
 
-    public static int getResourceId(Context c, int attr) {
+    @AnyRes
+    public static int getResourceId(@NonNull Context c, @AttrRes int attr) {
         TypedValue typedValue = new TypedValue();
         c.getTheme().resolveAttribute(attr, typedValue, true);
         int[] attrArray = new int[]{attr};
@@ -77,23 +80,21 @@ public class ThemeUtil {
         return resId;
     }
 
-    public static void theme(NumberPicker numberPicker) {
+    public static void theme(@NonNull NumberPicker numberPicker) {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            if (numberPicker != null) {
-                final int count = numberPicker.getChildCount();
-                for (int i = 0; i < count; i++) {
-                    View child = numberPicker.getChildAt(i);
-                    if (child instanceof EditText) {
-                        try {
-                            Field selectorWheelPaintField = numberPicker.getClass().getDeclaredField("mSelectorWheelPaint");
-                            selectorWheelPaintField.setAccessible(true);
-                            int colorAccent = numberPicker.getResources().getColor(ThemeUtil.getResourceId(numberPicker.getContext(), R.attr.colorAccent));
-                            ((Paint) selectorWheelPaintField.get(numberPicker)).setColor(colorAccent);
-                            ((EditText) child).setTextColor(colorAccent);
-                            numberPicker.invalidate();
-                        } catch (NoSuchFieldException | IllegalAccessException | IllegalArgumentException e) {
-                            e.printStackTrace();
-                        }
+            final int count = numberPicker.getChildCount();
+            for (int i = 0; i < count; i++) {
+                View child = numberPicker.getChildAt(i);
+                if (child instanceof EditText) {
+                    try {
+                        Field selectorWheelPaintField = numberPicker.getClass().getDeclaredField("mSelectorWheelPaint");
+                        selectorWheelPaintField.setAccessible(true);
+                        int colorAccent = numberPicker.getResources().getColor(ThemeUtil.getResourceId(numberPicker.getContext(), R.attr.colorAccent));
+                        ((Paint) selectorWheelPaintField.get(numberPicker)).setColor(colorAccent);
+                        ((EditText) child).setTextColor(colorAccent);
+                        numberPicker.invalidate();
+                    } catch (NoSuchFieldException | IllegalAccessException | IllegalArgumentException e) {
+                        e.printStackTrace();
                     }
                 }
             }
