62f4607e3ed984b594564dbca62cf82d457e3bef
Paul Woitaschek <woitaschek@posteo.de>
2015-08-21 19:55:28 +0200

    Greatly improved the file comparation so it represents a natural ordering.

--- audiobook/src/main/java/de/ph1b/audiobook/model/NaturalOrderComparator.java
+++ audiobook/src/main/java/de/ph1b/audiobook/model/NaturalOrderComparator.java
@@ -1,7 +1,13 @@
 package de.ph1b.audiobook.model;
 
+
+import android.support.annotation.VisibleForTesting;
+
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 
 public class NaturalOrderComparator {
@@ -105,6 +111,7 @@ public class NaturalOrderComparator {
         }
     }
 
+    @VisibleForTesting
     private static int naturalCompare(File lhs, File rhs) {
         if (lhs.isDirectory() && !rhs.isDirectory()) {
             // Directory before non-directory
@@ -113,10 +120,46 @@ public class NaturalOrderComparator {
             // Non-directory after directory
             return 1;
         } else {
-            // Alphabetic order otherwise, ignoring Capital
-            String a = lhs.getName();
-            String b = rhs.getName();
-            return naturalCompare(a, b);
+
+            // make a list containing the file and all its parent directories
+            List<File> lhsParents = new ArrayList<>(10);
+            File lhsFile = lhs;
+            do {
+                lhsParents.add(lhsFile);
+            } while ((lhsFile = lhsFile.getParentFile()) != null);
+            List<File> rhsParents = new ArrayList<>(10);
+            File rhsFile = rhs;
+            do {
+                rhsParents.add(rhsFile);
+            }
+            while ((rhsFile = rhsFile.getParentFile()) != null);
+
+            // reverse the list so it starts with the topmost parent
+            Collections.reverse(lhsParents);
+            Collections.reverse(rhsParents);
+
+            // iterates beginning from the topmost parent folder and returns the calculated sorting
+            // if they differ
+            for (int i = 0; i < lhsParents.size() && i < rhsParents.size(); i++) {
+                File left = lhsParents.get(i);
+                File right = rhsParents.get(i);
+                if (!left.equals(right)) {
+                    return naturalCompare(left.getAbsolutePath(), right.getAbsolutePath());
+                }
+            }
+
+            if (lhsParents.size() == rhsParents.size()) {
+                // if the amount of folders matches the files are within the same directory.
+                int lastIndex = lhsParents.size() - 1;
+                return naturalCompare(lhsParents.get(lastIndex).getName(), rhsParents.get(lastIndex).getName());
+            } else if (lhsParents.size() < rhsParents.size()) {
+                // if the first element has more parents, return a 1 so elements with a lower
+                // hierarchy will be first
+                return 1;
+            } else {
+                // else return -1 so the elemnts with the larger hierarchy will be later
+                return -1;
+            }
         }
     }
 }
---
+++ audiobook/src/main/java/de/ph1b/audiobook/model/NaturalOrderComparatorTest.java
@@ -0,0 +1,52 @@
+package de.ph1b.audiobook.model;
+
+
+import junit.framework.TestCase;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+/**
+ * A simple test for the file comparator that sorts in a 'natural' way.
+ *
+ * @author Paul Woitaschek
+ */
+public class NaturalOrderComparatorTest extends TestCase {
+
+    private List<File> list;
+
+
+    /**
+     * Tests if the comparation results in the desired order.
+     */
+    public void testFileComparation() {
+        File f1 = new File("a.jpg");
+        File f2 = new File("b.jpg");
+        File f3 = new File("/folder/a.jpg");
+        File f4 = new File("/folder/b.jpg");
+        File f5 = new File("/xfolder/d.jpg");
+        File f6 = new File("/xfolder");
+        File f7 = new File("/folder");
+        File f8 = new File("Ab.jpg");
+        File f9 = new File("aC.jpg");
+        list = Arrays.asList(f1, f2, f3, f4, f5, f6, f7, f8, f9);
+        Collections.sort(list, NaturalOrderComparator.FILE_COMPARATOR);
+        System.out.println(list);
+
+        assertTrue(indexOf(f1) < indexOf(f2));
+        assertTrue(indexOf(f3) < indexOf(f1));
+        assertTrue(indexOf(f5) > indexOf(f3));
+        assertTrue(indexOf(f7) < indexOf(f5));
+        assertTrue(indexOf(f8) < indexOf(f9));
+        assertTrue(indexOf(f9) > indexOf(f1));
+        assertTrue(indexOf(f9) < indexOf(f2));
+    }
+
+    public int indexOf(File file) {
+        return list.indexOf(file);
+    }
+
+}
