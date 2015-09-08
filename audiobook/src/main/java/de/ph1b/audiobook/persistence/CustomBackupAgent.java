0173416447c605abc0f3b0085acc7475e31c13c3
Paul Woitaschek <woitaschek@posteo.de>
2015-09-08 16:31:53 +0200

    Revert "Removed acra"
    This reverts commit 1a9d3170993ebf94cc9f53b5c9573e275cd2a776.

--- audiobook/src/main/java/de/ph1b/audiobook/persistence/CustomBackupAgent.java
+++ audiobook/src/main/java/de/ph1b/audiobook/persistence/CustomBackupAgent.java
@@ -17,11 +17,13 @@ public class CustomBackupAgent extends BackupAgentHelper {
         String sleepTime = getString(R.string.pref_key_sleep_time);
         String theme = getString(R.string.pref_key_theme);
         String pauseOnCanDuck = getString(R.string.pref_key_pause_on_can_duck);
+        String autoBugReportEnable = "acra.enable";
         String autoRewind = getString(R.string.pref_key_auto_rewind);
         String bookmarkOnSleep = getString(R.string.pref_key_bookmark_on_sleep);
 
         SharedPreferencesBackupHelper helper = new SharedPreferencesBackupHelper(this,
-                resumeOnReplug, seekTime, sleepTime, theme, pauseOnCanDuck, autoRewind, bookmarkOnSleep);
+                resumeOnReplug, seekTime, sleepTime, theme, pauseOnCanDuck, autoBugReportEnable,
+                autoRewind, bookmarkOnSleep);
         addHelper(BACKUP_KEY, helper);
     }
 }
---
+++ audiobook/src/main/java/de/ph1b/audiobook/utils/BaseApplication.java
@@ -0,0 +1,30 @@
+package de.ph1b.audiobook.utils;
+
+import android.app.Application;
+
+import org.acra.ACRA;
+import org.acra.annotation.ReportsCrashes;
+import org.acra.sender.HttpSender;
+
+import de.ph1b.audiobook.BuildConfig;
+import de.ph1b.audiobook.model.BookAdder;
+
+@ReportsCrashes(
+        httpMethod = HttpSender.Method.PUT,
+        reportType = HttpSender.Type.JSON,
+        formUri = "http://acra-63e870.smileupps.com/acra-material/_design/acra-storage/_update/report",
+        formUriBasicAuthLogin = "defaultreporter",
+        formUriBasicAuthPassword = "KA0Kc8h4dV4lCZBz")
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (!BuildConfig.DEBUG) {
+            ACRA.init(this);
+        }
+
+        BookAdder.getInstance(this).scanForFiles(true);
+    }
+}
