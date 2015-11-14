1b09102ea10e047a6ec4cb3d0dc7153862a40c70
Paul Woitaschek <woitaschek@posteo.de>
2015-11-14 22:13:48 +0100

    Fixed issue with the comparator by using intellijs.

--- audiobook/src/main/java/de/ph1b/audiobook/model/NaturalOrderComparator.java
+++ audiobook/src/main/java/de/ph1b/audiobook/model/NaturalOrderComparator.java
@@ -7,7 +7,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-import de.ph1b.audiobook.utils.AlphaComparator;
+import de.ph1b.audiobook.utils.IntelliJStringComparator;
 
 
 /**
@@ -17,55 +17,42 @@ import de.ph1b.audiobook.utils.AlphaComparator;
  */
 public class NaturalOrderComparator {
 
-    public static final Comparator<String> STRING_COMPARATOR = new AlphaComparator();
+    public static final Comparator<String> STRING_COMPARATOR = new IntelliJStringComparator();
     public static final Comparator<File> FILE_COMPARATOR = (lhs, rhs) -> {
-        if (lhs.isDirectory() && !rhs.isDirectory()) {
-            // Directory before non-directory
-            return -1;
-        } else if (!lhs.isDirectory() && rhs.isDirectory()) {
-            // Non-directory after directory
-            return 1;
-        } else {
+        if (lhs.isDirectory() || rhs.isDirectory()) {
+            return lhs.compareTo(rhs);
+        }
 
-            // make a list containing the file and all its parent directories
-            List<File> lhsParents = new ArrayList<>(10);
-            File lhsFile = lhs;
-            do {
-                lhsParents.add(lhsFile);
-            } while ((lhsFile = lhsFile.getParentFile()) != null);
-            List<File> rhsParents = new ArrayList<>(10);
-            File rhsFile = rhs;
-            do {
-                rhsParents.add(rhsFile);
-            }
-            while ((rhsFile = rhsFile.getParentFile()) != null);
+        List<File> left = getFileWithParents(lhs);
+        List<File> right = getFileWithParents(rhs);
 
-            // reverse the list so it starts with the topmost parent
-            Collections.reverse(lhsParents);
-            Collections.reverse(rhsParents);
+        int leftSize = left.size();
+        int rightSize = right.size();
 
-            // iterates beginning from the topmost parent folder and returns the calculated sorting
-            // if they differ
-            for (int i = 0; i < lhsParents.size() && i < rhsParents.size(); i++) {
-                File left = lhsParents.get(i);
-                File right = rhsParents.get(i);
-                if (!left.equals(right)) {
-                    return STRING_COMPARATOR.compare(left.getAbsolutePath(), right.getAbsolutePath());
-                }
-            }
-
-            if (lhsParents.size() == rhsParents.size()) {
-                // if the amount of folders matches the files are within the same directory.
-                int lastIndex = lhsParents.size() - 1;
-                return STRING_COMPARATOR.compare(lhsParents.get(lastIndex).getName(), rhsParents.get(lastIndex).getName());
-            } else if (lhsParents.size() < rhsParents.size()) {
-                // if the first element has more parents, return a 1 so elements with a lower
-                // hierarchy will be first
-                return 1;
-            } else {
-                // else return -1 so the elemnts with the larger hierarchy will be later
-                return -1;
+        // compare parents only and return if one differs
+        for (int i = 0, toLeft = leftSize - 1, toRight = rightSize - 1; i < toLeft && i < toRight; i++) {
+            String pl = left.get(i).getName();
+            String pr = right.get(i).getName();
+            if (!pl.equals(pr)) {
+                return STRING_COMPARATOR.compare(pl, pr);
             }
         }
+
+        // if sizes are the same
+        if (leftSize == rightSize) {
+            return STRING_COMPARATOR.compare(lhs.getName(), rhs.getName());
+        } else {
+            return rightSize - leftSize;
+        }
     };
+
+    private static List<File> getFileWithParents(File target) {
+        List<File> all = new ArrayList<>(10);
+        File current = target;
+        do {
+            all.add(current);
+        } while ((current = current.getParentFile()) != null);
+        Collections.reverse(all);
+        return all;
+    }
 }
---
+++ audiobook/src/main/java/de/ph1b/audiobook/utils/IntelliJStringComparator.java
@@ -0,0 +1,129 @@
+package de.ph1b.audiobook.utils;
+
+import android.support.annotation.Nullable;
+
+import java.util.Comparator;
+
+/**
+ * Simple copy of IntelliJs-Community naturalCompare (in StringUtil).
+ * Licensed as Apache v2.
+ *
+ * @author Paul Woitaschek
+ */
+public class IntelliJStringComparator implements Comparator<String> {
+
+    public static boolean isDecimalDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    public static char toUpperCase(char a) {
+        if (a < 'a') {
+            return a;
+        }
+        if (a <= 'z') {
+            return (char) (a + ('A' - 'a'));
+        }
+        return Character.toUpperCase(a);
+    }
+
+
+    public static int naturalCompare(@Nullable String string1, @Nullable String string2, boolean caseSensitive) {
+        //noinspection StringEquality
+        if (string1 == string2) {
+            return 0;
+        }
+        if (string1 == null) {
+            return -1;
+        }
+        if (string2 == null) {
+            return 1;
+        }
+
+        final int string1Length = string1.length();
+        final int string2Length = string2.length();
+        int i = 0;
+        int j = 0;
+        for (; i < string1Length && j < string2Length; i++, j++) {
+            char ch1 = string1.charAt(i);
+            char ch2 = string2.charAt(j);
+            if ((isDecimalDigit(ch1) || ch1 == ' ') && (isDecimalDigit(ch2) || ch2 == ' ')) {
+                int startNum1 = i;
+                while (ch1 == ' ' || ch1 == '0') { // skip leading spaces and zeros
+                    startNum1++;
+                    if (startNum1 >= string1Length) break;
+                    ch1 = string1.charAt(startNum1);
+                }
+                int startNum2 = j;
+                while (ch2 == ' ' || ch2 == '0') { // skip leading spaces and zeros
+                    startNum2++;
+                    if (startNum2 >= string2Length) break;
+                    ch2 = string2.charAt(startNum2);
+                }
+                i = startNum1;
+                j = startNum2;
+                // find end index of number
+                while (i < string1Length && isDecimalDigit(string1.charAt(i))) i++;
+                while (j < string2Length && isDecimalDigit(string2.charAt(j))) j++;
+                final int lengthDiff = (i - startNum1) - (j - startNum2);
+                if (lengthDiff != 0) {
+                    // numbers with more digits are always greater than shorter numbers
+                    return lengthDiff;
+                }
+                for (; startNum1 < i; startNum1++, startNum2++) {
+                    // compare numbers with equal digit count
+                    final int diff = string1.charAt(startNum1) - string2.charAt(startNum2);
+                    if (diff != 0) {
+                        return diff;
+                    }
+                }
+                i--;
+                j--;
+            } else {
+                if (caseSensitive) {
+                    return ch1 - ch2;
+                } else {
+                    // similar logic to charsMatch() below
+                    if (ch1 != ch2) {
+                        final int diff1 = toUpperCase(ch1) - toUpperCase(ch2);
+                        if (diff1 != 0) {
+                            final int diff2 = toLowerCase(ch1) - toLowerCase(ch2);
+                            if (diff2 != 0) {
+                                return diff2;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // After the loop the end of one of the strings might not have been reached, if the other
+        // string ends with a number and the strings are equal until the end of that number. When
+        // there are more characters in the string, then it is greater.
+        if (i < string1Length) {
+            return 1;
+        }
+        if (j < string2Length) {
+            return -1;
+        }
+        if (!caseSensitive && string1Length == string2Length) {
+            // do case sensitive compare if case insensitive strings are equal
+            return naturalCompare(string1, string2, true);
+        }
+        return string1Length - string2Length;
+    }
+
+    public static char toLowerCase(char a) {
+        if (a < 'A' || a >= 'a' && a <= 'z') {
+            return a;
+        }
+        if (a <= 'Z') {
+            return (char) (a + ('a' - 'A'));
+        }
+
+        return Character.toLowerCase(a);
+    }
+
+    @Override
+    public int compare(String lhs, String rhs) {
+        return naturalCompare(lhs, rhs, false);
+    }
+}
