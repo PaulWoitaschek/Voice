0d7620c7578b95a84705daeb2110e2ae2784d7d5
Paul Woitaschek <woitaschek@posteo.de>
2015-11-26 21:49:39 +0100

    Rewritten interfaces package to kotlin

---
+++ audiobook/src/main/kotlin/de/ph1b/audiobook/interfaces/MultiPaneInformer.kt
@@ -0,0 +1,14 @@
+package de.ph1b.audiobook.interfaces
+
+/**
+ * A basic interface that informs if the current layout mode has multiple panels.
+
+ * @author Paul Woitaschek
+ */
+interface MultiPaneInformer {
+
+    /**
+     * true if the layout has multiple panels.
+     */
+    val isMultiPanel: Boolean
+}
