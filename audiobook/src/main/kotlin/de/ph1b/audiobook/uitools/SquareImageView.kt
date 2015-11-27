524cbfe0369fc328b1a54cc895a7502a5392cb92
Paul Woitaschek <woitaschek@posteo.de>
2015-11-27 13:18:33 +0100

    Rewritten SquareImageView in kotlin

---
+++ audiobook/src/main/kotlin/de/ph1b/audiobook/uitools/SquareImageView.kt
@@ -0,0 +1,21 @@
+package de.ph1b.audiobook.uitools
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ImageView
+
+class SquareImageView : ImageView {
+    constructor(context: Context) : super(context) {
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        setMeasuredDimension(measuredWidth, measuredWidth)
+    }
+}
