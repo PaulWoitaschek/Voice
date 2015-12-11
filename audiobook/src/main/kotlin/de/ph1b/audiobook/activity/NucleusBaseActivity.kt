ec8b3c0e5dfa3fe40d2dad2c50f51bf16daaf6e2
Paul Woitaschek <woitaschek@posteo.de>
2015-12-12 00:41:19 +0100

    Added copyright notices for activity package

--- audiobook/src/main/kotlin/de/ph1b/audiobook/activity/NucleusBaseActivity.kt
+++ audiobook/src/main/kotlin/de/ph1b/audiobook/activity/NucleusBaseActivity.kt
@@ -1,3 +1,37 @@
+/*
+ * This file is part of Material Audiobook Player.
+ *
+ * Material Audiobook Player is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ * Material Audiobook Player is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * /licenses/>.
+ */
+
+/*
+ * This file is part of Material Audiobook Player.
+ *
+ * Material Audiobook Player is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ * Material Audiobook Player is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * /licenses/>.
+ */
+
 package de.ph1b.audiobook.activity
 
 import android.content.Intent
@@ -31,7 +65,7 @@ abstract class NucleusBaseActivity <P : Presenter<out Any>> : NucleusAppCompatAc
 
 
     @CallSuper
-  open  fun onResume(subscription: CompositeSubscription) {
+    open fun onResume(subscription: CompositeSubscription) {
 
     }
 
