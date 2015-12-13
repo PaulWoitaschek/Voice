2b243f3b852d44c24f16b163ba528d0e1e86795b
Paul Woitaschek <woitaschek@posteo.de>
2015-12-13 12:20:36 +0100

    Added missing base fragment.

---
+++ audiobook/src/main/kotlin/de/ph1b/audiobook/fragment/NucleusBaseFragment.kt
@@ -0,0 +1,53 @@
+/*
+ * This file is part of Material Audiobook Player.
+ *
+ * Material Audiobook Player is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ * Material Audiobook Player is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * /licenses/>.
+ */
+
+/*
+ * This file is part of Material Audiobook Player.
+ *
+ * Material Audiobook Player is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ * Material Audiobook Player is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * /licenses/>.
+ */
+
+package de.ph1b.audiobook.fragment
+
+import de.ph1b.audiobook.injection.App
+import nucleus.presenter.Presenter
+import nucleus.view.NucleusSupportFragment
+
+/**
+ * Base fragment for nucleus that enables leak watching.
+ *
+ * @author Paul Woitaschek
+ */
+open class NucleusBaseFragment<P : Presenter<out Any>> : NucleusSupportFragment <P>() {
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        App.leakWatch(this)
+    }
+}
