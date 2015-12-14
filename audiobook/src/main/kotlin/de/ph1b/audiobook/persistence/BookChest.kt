702e3a17bb5044f5722dfad4f3290a55080652ba
Paul Woitaschek <woitaschek@posteo.de>
2015-12-14 22:10:22 +0100

    Replace use function with cursor loop inline function extension.

--- audiobook/src/main/kotlin/de/ph1b/audiobook/persistence/BookChest.kt
+++ audiobook/src/main/kotlin/de/ph1b/audiobook/persistence/BookChest.kt
@@ -32,6 +32,23 @@
  * /licenses/>.
  */
 
+/*
+ * This file is part of Material Audiobook Player.
+ *
+ * Material Audiobook Player is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ * Material Audiobook Player is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * /licenses/>.
+ */
+
 package de.ph1b.audiobook.persistence
 
 import android.content.ContentValues
@@ -66,11 +83,9 @@ constructor(c: Context) {
         synchronized(this) {
             val cursor = db.rawQuery("$FULL_PROJECTION $APPEND_WHERE_ACTIVE", arrayOf(BOOLEAN_TRUE.toString()))
             val active = ArrayList<Book>(cursor.count)
-            cursor.use {
-                while (cursor.moveToNext()) {
-                    val book = byProjection(cursor)
-                    active.add(book)
-                }
+            cursor.moveToNextLoop {
+                val book = byProjection(this)
+                active.add(book)
             }
             active
         }
@@ -80,11 +95,9 @@ constructor(c: Context) {
         synchronized(this) {
             val cursor = db.rawQuery("$FULL_PROJECTION $APPEND_WHERE_ACTIVE", arrayOf(BOOLEAN_FALSE.toString()))
             val active = ArrayList<Book>(cursor.count)
-            cursor.use {
-                while (cursor.moveToNext()) {
-                    val book = byProjection(cursor)
-                    active.add(book)
-                }
+            cursor.moveToNextLoop {
+                val book = byProjection(cursor)
+                active.add(book)
             }
             active
         }
--- audiobook/src/main/kotlin/de/ph1b/audiobook/persistence/DataBaseExtensions.kt
+++ audiobook/src/main/kotlin/de/ph1b/audiobook/persistence/DataBaseExtensions.kt
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Material Audiobook Player.
+ *
+ * Material Audiobook Player is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ * Material Audiobook Player is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * /licenses/>.
+ */
+
 package de.ph1b.audiobook.persistence
 
 import android.content.SharedPreferences
@@ -38,6 +55,16 @@ inline fun SharedPreferences.edit(func: SharedPreferences.Editor.() -> Unit) {
     val editor = this.edit()
     editor.func()
     editor.apply()
+}
+
+inline fun Cursor.moveToNextLoop(func: Cursor.() -> Unit) {
+    try {
+        while (moveToNext()) {
+            func()
+        }
+    } finally {
+        close()
+    }
 }
 
 fun SharedPreferences.Editor.setString(pair: Pair<String, String>) =
