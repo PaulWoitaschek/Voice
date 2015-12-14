7a63041a2850f23c70fd70f3fdaba5db5104324b
Paul Woitaschek <woitaschek@posteo.de>
2015-12-14 22:41:17 +0100

    Sleep updater interval changed to 1s.

--- audiobook/src/main/kotlin/de/ph1b/audiobook/mediaplayer/MediaPlayerController.kt
+++ audiobook/src/main/kotlin/de/ph1b/audiobook/mediaplayer/MediaPlayerController.kt
@@ -15,6 +15,23 @@
  * /licenses/>.
  */
 
+/*
+ * This file is part of Material Audiobook Player.
+ *
+ * Material Audiobook Player is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ * Material Audiobook Player is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * /licenses/>.
+ */
+
 package de.ph1b.audiobook.mediaplayer
 
 import android.content.Context
@@ -191,7 +208,7 @@ constructor(private val c: Context, private val prefs: PrefsManager, private val
         subscriptions.apply {
             if (!hasSubscriptions()) {
                 // counts down the sleep sand
-                val sleepUpdateInterval = 500L
+                val sleepUpdateInterval = 1000L
                 add(Observable.interval(sleepUpdateInterval, TimeUnit.MILLISECONDS)
                         .filter { internalSleepSand.value > 0 } // only notify if there is still time left
                         .map { internalSleepSand.value - sleepUpdateInterval } // calculate the new time
