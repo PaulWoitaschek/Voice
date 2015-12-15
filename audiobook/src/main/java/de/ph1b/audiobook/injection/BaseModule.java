dc74a94b4897e9e4dcc2da1fe21c9a2b94da58a0
Paul Woitaschek <woitaschek@posteo.de>
2015-12-15 18:25:27 +0100

    Revert "Replaced buggy antenna player with our old solution ported to kotlin."
    This reverts commit 1c357efc0d61d218443e47bbe83f5f91387bb17f.

--- audiobook/src/main/java/de/ph1b/audiobook/injection/BaseModule.java
+++ audiobook/src/main/java/de/ph1b/audiobook/injection/BaseModule.java
@@ -1,22 +1,6 @@
-/*
- * This file is part of Material Audiobook Player.
- *
- * Material Audiobook Player is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or any later version.
- *
- * Material Audiobook Player is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
- * /licenses/>.
- */
-
 package de.ph1b.audiobook.injection;
 
+import android.content.Context;
 import android.os.Build;
 
 import java.util.Arrays;
@@ -27,7 +11,7 @@ import javax.inject.Singleton;
 import dagger.Module;
 import dagger.Provides;
 import de.ph1b.audiobook.mediaplayer.AndroidMediaPlayer;
-import de.ph1b.audiobook.mediaplayer.CustomMediaPlayer;
+import de.ph1b.audiobook.mediaplayer.AntennaPlayer;
 import de.ph1b.audiobook.mediaplayer.MediaPlayerInterface;
 import de.ph1b.audiobook.receiver.HeadsetPlugReceiver;
 import de.ph1b.audiobook.uitools.ImageLinkService;
@@ -64,11 +48,11 @@ public class BaseModule {
     }
 
     @Provides
-    MediaPlayerInterface provideMediaPlayer() {
+    MediaPlayerInterface provideMediaPlayer(Context context) {
         if (MIN_MARSHMALLOW || !canUseSonic()) {
             return new AndroidMediaPlayer();
         } else {
-            return new CustomMediaPlayer();
+            return new AntennaPlayer(context);
         }
     }
 
