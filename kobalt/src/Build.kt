72dccb47928c3bda5b08c658cbf8f30e835122e8
Paul Woitaschek <woitaschek@posteo.de>
2016-01-30 18:22:31 +0100

    Added missing imports

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -32,6 +32,28 @@
  * /licenses/>.
  */
 
+/*
+ * This file is part of Material Audiobook Player.
+ *
+ * Material Audiobook Player is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ * Material Audiobook Player is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Material Audiobook Player. If not, see <http://www.gnu.org/licenses/>.
+ * /licenses/>.
+ */
+
+import com.beust.kobalt.plugin.java.javaProject
+import com.beust.kobalt.plugin.packaging.assemble
+import com.beust.kobalt.plugins
+import com.beust.kobalt.repos
+
 val pl = plugins("com.beust:kobalt-android:")
 
 val repos = repos("https://jitpack.io")
