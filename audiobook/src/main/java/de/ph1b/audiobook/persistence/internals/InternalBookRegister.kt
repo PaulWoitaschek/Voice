4399fe0098226e691678e6adbfd586931cb22425
Paul Woitaschek <woitaschek@posteo.de>
2016-03-09 00:33:12 +0100

    Removed anko sql

--- audiobook/src/main/java/de/ph1b/audiobook/persistence/internals/InternalBookRegister.kt
+++ audiobook/src/main/java/de/ph1b/audiobook/persistence/internals/InternalBookRegister.kt
@@ -21,8 +21,8 @@ import android.content.ContentValues
 import android.database.sqlite.SQLiteDatabase
 import de.ph1b.audiobook.model.Book
 import de.ph1b.audiobook.model.Chapter
-import org.jetbrains.anko.db.*
 import java.io.File
+import java.util.*
 import javax.inject.Inject
 
 
@@ -36,53 +36,66 @@ class InternalBookRegister
 
     private val db by lazy { internalDb.writableDatabase }
 
-    private fun books(active: Boolean) = db.select(BookTable.TABLE_NAME)
-            .whereSimple("${BookTable.ACTIVE} =?", if (active) "1" else "0")
-            .columns(BookTable.ID,
-                    BookTable.NAME,
-                    BookTable.AUTHOR,
-                    BookTable.CURRENT_MEDIA_PATH,
-                    BookTable.PLAYBACK_SPEED,
-                    BookTable.ROOT,
-                    BookTable.TIME,
-                    BookTable.TYPE,
-                    BookTable.USE_COVER_REPLACEMENT)
-            .parseList(rowParser { bookId: Long,
-                                   bookName: String,
-                                   bookAuthor: String?,
-                                   currentPath: String,
-                                   bookSpeed: Float,
-                                   bookRoot: String,
-                                   bookTime: Int,
-                                   bookType: String,
-                                   bookUseCoverReplacement: Int ->
-                val chapters = db.select(ChapterTable.TABLE_NAME)
-                        .columns(ChapterTable.NAME, ChapterTable.DURATION, ChapterTable.PATH)
-                        .whereSimple("${ChapterTable.BOOK_ID} =?", bookId.toString())
-                        .parseList(rowParser { name: String, duration: Int, path: String ->
-                            Chapter(File(path), name, duration)
-                        })
+    private fun books(active: Boolean) = db.asTransaction {
+        val cursor = db.simpleQuery(table = BookTable.TABLE_NAME,
+                columns = arrayOf(BookTable.ID,
+                        BookTable.NAME,
+                        BookTable.AUTHOR,
+                        BookTable.CURRENT_MEDIA_PATH,
+                        BookTable.PLAYBACK_SPEED,
+                        BookTable.ROOT,
+                        BookTable.TIME,
+                        BookTable.TYPE,
+                        BookTable.USE_COVER_REPLACEMENT),
+                selection = "${BookTable.ACTIVE} =?",
+                selectionArgs = arrayOf(if (active) "1" else "0")
+        )
+        val books = ArrayList<Book>(cursor.count)
+        cursor.moveToNextLoop {
+            val bookId: Long = long(BookTable.ID)
+            val bookName: String = string(BookTable.NAME)
+            val bookAuthor: String? = stringNullable(BookTable.AUTHOR)
+            val currentPath: String = string(BookTable.CURRENT_MEDIA_PATH)
+            val bookSpeed: Float = float(BookTable.PLAYBACK_SPEED)
+            val bookRoot: String = string(BookTable.ROOT)
+            val bookTime: Int = int(BookTable.TIME)
+            val bookType: String = string(BookTable.TYPE)
+            val bookUseCoverReplacement: Int = int(BookTable.USE_COVER_REPLACEMENT)
 
-                return@rowParser Book(bookId,
-                        Book.Type.valueOf(bookType),
-                        bookUseCoverReplacement == 1,
-                        bookAuthor,
-                        File(currentPath),
-                        bookTime,
-                        bookName,
-                        chapters,
-                        bookSpeed,
-                        bookRoot)
-            })
+            val chapterCursor = db.simpleQuery(table = ChapterTable.TABLE_NAME,
+                    columns = arrayOf(ChapterTable.NAME, ChapterTable.DURATION, ChapterTable.PATH),
+                    selection = "${ChapterTable.BOOK_ID} =?",
+                    selectionArgs = arrayOf(bookId.toString()))
+            val chapters = ArrayList<Chapter>(chapterCursor.count)
+            chapterCursor.moveToNextLoop {
+                val name: String = string(ChapterTable.NAME)
+                val duration: Int = int(ChapterTable.DURATION)
+                val path: String = string(ChapterTable.PATH)
+                chapters.add(Chapter(File(path), name, duration))
+            }
+
+            books.add(Book(bookId,
+                    Book.Type.valueOf(bookType),
+                    bookUseCoverReplacement == 1,
+                    bookAuthor,
+                    File(currentPath),
+                    bookTime,
+                    bookName,
+                    chapters,
+                    bookSpeed,
+                    bookRoot))
+        }
+        return@asTransaction books
+    }
 
     fun activeBooks() = books(true)
 
     fun orphanedBooks() = books(false)
 
-    private fun setBookVisible(bookId: Long, visible: Boolean) = db.update(BookTable.TABLE_NAME, BookTable.ACTIVE to if (visible) 1 else 0)
-            .whereSimple("${BookTable.ID} =?", bookId.toString())
-            .exec()
-
+    private fun setBookVisible(bookId: Long, visible: Boolean) = db.update(BookTable.TABLE_NAME,
+            ContentValues().apply {
+                put(BookTable.ACTIVE, if (visible) 1 else 0)
+            }, "${BookTable.ID} =?", arrayOf(bookId.toString()))
 
     fun revealBook(bookId: Long) {
         setBookVisible(bookId, true)
@@ -92,13 +105,17 @@ class InternalBookRegister
         setBookVisible(bookId, false)
     }
 
-    private fun SQLiteDatabase.insert(chapter: Chapter, bookId: Long) = insert(ChapterTable.TABLE_NAME,
-            ChapterTable.DURATION to chapter.duration,
-            ChapterTable.NAME to chapter.name,
-            ChapterTable.PATH to chapter.file.absolutePath,
-            ChapterTable.BOOK_ID to bookId)
+    private fun SQLiteDatabase.insert(chapter: Chapter, bookId: Long) =
+            insert(ChapterTable.TABLE_NAME, null, chapter.toContentValues(bookId))
 
-    private fun Book.getContentValues() = ContentValues().apply {
+    private fun Chapter.toContentValues(bookId: Long) = ContentValues().apply {
+        put(ChapterTable.DURATION, duration)
+        put(ChapterTable.NAME, name)
+        put(ChapterTable.PATH, file.absolutePath)
+        put(ChapterTable.BOOK_ID, bookId)
+    }
+
+    private fun Book.toContentValues() = ContentValues().apply {
         put(BookTable.NAME, name)
         put(BookTable.AUTHOR, author)
         put(BookTable.ACTIVE, 1)
@@ -110,9 +127,9 @@ class InternalBookRegister
         put(BookTable.USE_COVER_REPLACEMENT, useCoverReplacement)
     }
 
-    fun updateBook(book: Book) = db.transaction {
+    fun updateBook(book: Book) = db.asTransaction {
         // update book itself
-        val bookCv = book.getContentValues()
+        val bookCv = book.toContentValues()
         update(BookTable.TABLE_NAME, bookCv, "${BookTable.ID}=?", arrayOf(book.id.toString()))
 
         // delete old chapters and replace them with new ones
@@ -121,7 +138,7 @@ class InternalBookRegister
     }
 
     fun addBook(toAdd: Book) = db.asTransaction {
-        val bookCv = toAdd.getContentValues()
+        val bookCv = toAdd.toContentValues()
         val bookId = insert(BookTable.TABLE_NAME, null, bookCv)
         val newBook = toAdd.copy(id = bookId)
         newBook.chapters.forEach { insert(it, bookId) }
