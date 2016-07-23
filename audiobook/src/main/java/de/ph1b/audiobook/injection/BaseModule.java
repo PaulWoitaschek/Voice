27d51f83b656cc273af23269093ede9ca5203d63
Paul Woitaschek <woitaschek@posteo.de>
2016-07-23 14:57:20 +0200

    Inline the player

--- audiobook/src/main/java/de/ph1b/audiobook/injection/BaseModule.java
+++ audiobook/src/main/java/de/ph1b/audiobook/injection/BaseModule.java
@@ -1,20 +1,3 @@
-/*
- * This file is part of Material Audiobook Player.
- *
- * Material Audiobook Player is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or any later version.
- *
- * Material Audiobook Player is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Material Audiobook Player. If not, see <http://www.gnu.org/licenses/>.
- * /licenses/>.
- */
-
 package de.ph1b.audiobook.injection;
 
 import android.content.Context;
@@ -26,10 +9,10 @@ import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
-import de.paul_woitaschek.mediaplayer.AndroidPlayer;
-import de.paul_woitaschek.mediaplayer.AntennaPlayer;
-import de.paul_woitaschek.mediaplayer.MediaPlayer;
 import de.ph1b.audiobook.persistence.PrefsManager;
+import de.ph1b.audiobook.playback.player.AndroidPlayer;
+import de.ph1b.audiobook.playback.player.AntennaPlayer;
+import de.ph1b.audiobook.playback.player.Player;
 import de.ph1b.audiobook.playback.utils.MediaPlayerCapabilities;
 
 /**
@@ -55,7 +38,7 @@ public class BaseModule {
 
    @Provides
    @Singleton
-   static MediaPlayer providePlayer(MediaPlayerCapabilities capabilities, Context context) {
+   static Player providePlayer(MediaPlayerCapabilities capabilities, Context context) {
       if (capabilities.getUseCustomMediaPlayer()) {
          return new AntennaPlayer(context);
       } else {
