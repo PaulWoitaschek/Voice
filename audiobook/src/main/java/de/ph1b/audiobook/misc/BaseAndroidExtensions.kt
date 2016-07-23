e2253cde76d7c15950450fe69f1d348755e0a83a
Paul Woitaschek <woitaschek@posteo.de>
2016-07-23 22:44:25 +0200

    Just pass the id, not an overcomplicated data class

--- audiobook/src/main/java/de/ph1b/audiobook/misc/BaseAndroidExtensions.kt
+++ audiobook/src/main/java/de/ph1b/audiobook/misc/BaseAndroidExtensions.kt
@@ -1,27 +1,7 @@
-/*
- * This file is part of Material Audiobook Player.
- *
- * Material Audiobook Player is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or any later version.
- *
- * Material Audiobook Player is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Material Audiobook Player. If not, see <http://www.gnu.org/licenses/>.
- * /licenses/>.
- */
-
 package de.ph1b.audiobook.misc
 
-import android.app.Activity
 import android.content.Context
-import android.content.Intent
 import android.graphics.drawable.Drawable
-import android.os.Bundle
 import android.support.annotation.ColorInt
 import android.support.annotation.ColorRes
 import android.support.annotation.DrawableRes
@@ -39,13 +19,6 @@ import android.widget.TextView
 
 val Fragment.actionBar: ActionBar
     get() = (activity as AppCompatActivity).supportActionBar!!
-
-inline fun <reified T : Activity> Activity.startActivity(args: Bundle? = null, flags: Int? = null) {
-    val intent = Intent(this, T::class.java)
-    args?.let { intent.putExtras(args) }
-    flags?.let { intent.flags = flags }
-    startActivity(intent)
-}
 
 fun Context.layoutInflater(): LayoutInflater = LayoutInflater.from(this)
 
