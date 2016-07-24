5d31597f89b092ee8ede34059db0f9b0d06aec39
Paul Woitaschek <woitaschek@posteo.de>
2016-07-24 20:46:37 +0200

    Added a splash activity for cold starts.

---
+++ audiobook/src/main/java/de/ph1b/audiobook/features/coldStart/SplashActivity.kt
@@ -0,0 +1,22 @@
+package de.ph1b.audiobook.features.coldStart
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import de.ph1b.audiobook.features.BookActivity
+
+/**
+ * Activity that just exists to fake a toolbar through its windowbackground upon start
+ *
+ * @author Paul Woitaschek
+ */
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this, BookActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
