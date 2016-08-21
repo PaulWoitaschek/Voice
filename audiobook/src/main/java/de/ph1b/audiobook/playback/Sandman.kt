a0667e3fe8f0515c4188554b110f0e705acd6acb
Paul Woitaschek <woitaschek@posteo.de>
2016-08-21 17:15:28 +0200

    Enable shake to reset if setting is set

--- audiobook/src/main/java/de/ph1b/audiobook/playback/Sandman.kt
+++ audiobook/src/main/java/de/ph1b/audiobook/playback/Sandman.kt
@@ -1,5 +1,6 @@
 package de.ph1b.audiobook.playback
 
+import d
 import de.ph1b.audiobook.misc.value
 import de.ph1b.audiobook.persistence.PrefsManager
 import i
@@ -15,7 +16,7 @@ import javax.inject.Singleton
  * Manages everything sleep related.
  */
 @Singleton class Sandman
-@Inject constructor(playerController: PlayerController, playStateManager: PlayStateManager, private val prefsManager: PrefsManager) {
+@Inject constructor(private val playerController: PlayerController, playStateManager: PlayStateManager, private val prefsManager: PrefsManager, shakeDetector: ShakeDetector) {
 
     /**
      * The time left till the playback stops in ms. If this is -1 the timer was stopped manually.
@@ -23,11 +24,16 @@ import javax.inject.Singleton
      */
     private val internalSleepSand = BehaviorSubject.create<Long>(-1L)
     private var sleepSubscription: Subscription? = null
+    private var shakeSubscription: Subscription? = null
+    private val shakeObservable = shakeDetector.create()
 
     init {
         // stops the player when the timer reaches 0
         internalSleepSand.filter { it == 0L } // when this reaches 0
-                .subscribe { playerController.stop() } // stop the player
+                .subscribe {
+                    // stop the player
+                    playerController.stop()
+                }
 
         // counts down the sleep sand
         val sleepUpdateInterval = 1000L
@@ -42,6 +48,7 @@ import javax.inject.Singleton
                                 .map { it.coerceAtLeast(0) } // but keep at least 0
                                 .subscribe { internalSleepSand.onNext(it) }
                     } else {
+                        pauseOnShake(false)
                         sleepSubscription?.unsubscribe()
                     }
                 }
@@ -50,7 +57,8 @@ import javax.inject.Singleton
     /** turns the sleep timer on or off **/
     fun setActive(enable: Boolean) {
         i { "toggleSleepSand. Left sleepTime is ${internalSleepSand.value}" }
-        if (sleepTimerActive() == enable) return
+
+        pauseOnShake(enable)
 
         if (enable) {
             i { "Starting sleepTimer" }
@@ -60,6 +68,21 @@ import javax.inject.Singleton
             i { "Cancelling sleepTimer" }
             internalSleepSand.onNext(-1L)
         }
+    }
+
+    private fun pauseOnShake(enabled: Boolean) {
+        if (enabled) {
+            val shouldSubscribe = shakeSubscription?.isUnsubscribed ?: true
+            if (shouldSubscribe) {
+                // setup shake detection if requested
+                if (prefsManager.shakeToReset.value()) {
+                    shakeSubscription = shakeObservable.subscribe {
+                        d { "reset now by shake" }
+                        setActive(true)
+                    }
+                }
+            }
+        } else shakeSubscription?.unsubscribe()
     }
 
     /**
