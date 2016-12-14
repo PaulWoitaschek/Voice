12458677595247b7c5535af0db0308f51b190e9c
Paul Woitaschek <woitaschek@gmail.com>
2016-12-14 21:27:14 +0100

    Removed unused extension function

--- audiobook/src/main/java/de/ph1b/audiobook/misc/rxInteropExtensions.kt
+++ audiobook/src/main/java/de/ph1b/audiobook/misc/rxInteropExtensions.kt
@@ -2,8 +2,6 @@ package de.ph1b.audiobook.misc
 
 import com.f2prateek.rx.preferences.Preference
 import hu.akarnokd.rxjava.interop.RxJavaInterop
-import io.reactivex.BackpressureStrategy
 
 fun <T> rx.Observable<T>.toV2Observable(): io.reactivex.Observable<T> = RxJavaInterop.toV2Observable(this)
-fun <T> io.reactivex.ObservableSource<T>.toV1Observable(): rx.Observable<T> = RxJavaInterop.toV1Observable(this, BackpressureStrategy.MISSING)
 fun <T> Preference<T>.asV2Observable() = asObservable().toV2Observable()
