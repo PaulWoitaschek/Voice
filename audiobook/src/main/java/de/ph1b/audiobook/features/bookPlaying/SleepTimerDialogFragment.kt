dd9e3f2822b23ea6f3f0338c58ac96ec76409221
Paul Woitaschek <woitaschek@posteo.de>
2017-02-14 20:25:22 +0100

    Made the sleep timer a bottom sheet dialog with a keypad

--- audiobook/src/main/java/de/ph1b/audiobook/features/bookPlaying/SleepTimerDialogFragment.kt
+++ audiobook/src/main/java/de/ph1b/audiobook/features/bookPlaying/SleepTimerDialogFragment.kt
@@ -1,19 +1,20 @@
 package de.ph1b.audiobook.features.bookPlaying
 
+import android.annotation.SuppressLint
 import android.app.Dialog
 import android.os.Bundle
-import android.support.v4.app.DialogFragment
+import android.support.design.widget.BottomSheetDialog
+import android.support.design.widget.FloatingActionButton
+import android.support.v7.app.AppCompatDialogFragment
 import android.support.v7.widget.SwitchCompat
 import android.text.format.DateUtils
-import android.widget.SeekBar
+import android.view.View
 import android.widget.TextView
-import com.afollestad.materialdialogs.MaterialDialog
 import de.ph1b.audiobook.Book
 import de.ph1b.audiobook.R
 import de.ph1b.audiobook.injection.App
+import de.ph1b.audiobook.misc.find
 import de.ph1b.audiobook.misc.layoutInflater
-import de.ph1b.audiobook.misc.onProgressChanged
-import de.ph1b.audiobook.misc.positiveClicked
 import de.ph1b.audiobook.misc.value
 import de.ph1b.audiobook.persistence.BookRepository
 import de.ph1b.audiobook.persistence.BookmarkProvider
@@ -29,7 +30,7 @@ import javax.inject.Inject
  *
  * @author Paul Woitaschek
  */
-class SleepTimerDialogFragment : DialogFragment() {
+class SleepTimerDialogFragment : AppCompatDialogFragment() {
 
   @Inject lateinit var bookmarkProvider: BookmarkProvider
   @Inject lateinit var prefs: PrefsManager
@@ -37,15 +38,64 @@ class SleepTimerDialogFragment : DialogFragment() {
   @Inject lateinit var repo: BookRepository
   @Inject lateinit var shakeDetector: ShakeDetector
 
+  private lateinit var time: TextView
+  private lateinit var fab: FloatingActionButton
+  private var selectedMinutes = 0
+
+  override fun onSaveInstanceState(outState: Bundle) {
+    super.onSaveInstanceState(outState)
+    outState.putInt(SI_MINUTES, selectedMinutes)
+  }
+
+  override fun onViewStateRestored(savedInstanceState: Bundle?) {
+    super.onViewStateRestored(savedInstanceState)
+    selectedMinutes = savedInstanceState?.getInt(SI_MINUTES) ?: prefs.seekTime.get()!!
+    updateTimeState()
+  }
+
+  @SuppressLint("SetTextI18n")
+  private fun appendNumber(number: Int) {
+    val newNumber = selectedMinutes * 10 + number
+    if (newNumber > 999) return
+    selectedMinutes = newNumber
+    updateTimeState()
+  }
+
+  private fun updateTimeState() {
+    time.text = getString(R.string.min, selectedMinutes.toString())
+
+    if (selectedMinutes > 0) fab.show()
+    else fab.hide()
+  }
+
   override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
     App.component.inject(this)
 
-    @SuppressWarnings("InflateParams")
+    @SuppressLint("InflateParams")
     val layout = context.layoutInflater().inflate(R.layout.dialog_sleep, null)
-    val seekBar = layout.findViewById(R.id.seekBar) as SeekBar
-    val textView = layout.findViewById(R.id.text) as TextView
     val bookmarkSwitch = layout.findViewById(R.id.bookmarkSwitch) as SwitchCompat
     val shakeToResetSwitch = layout.findViewById(R.id.shakeToResetSwitch) as SwitchCompat
+    fab = layout.find(R.id.fab)
+    time = layout.find(R.id.time)
+
+    // restore or get fresh
+    selectedMinutes = savedInstanceState?.getInt(SI_MINUTES) ?: prefs.seekTime.get()!!
+    updateTimeState()
+
+    layout.findViewById(R.id.one).setOnClickListener { appendNumber(1) }
+    layout.findViewById(R.id.two).setOnClickListener { appendNumber(2) }
+    layout.findViewById(R.id.three).setOnClickListener { appendNumber(3) }
+    layout.findViewById(R.id.four).setOnClickListener { appendNumber(4) }
+    layout.findViewById(R.id.five).setOnClickListener { appendNumber(5) }
+    layout.findViewById(R.id.six).setOnClickListener { appendNumber(6) }
+    layout.findViewById(R.id.seven).setOnClickListener { appendNumber(7) }
+    layout.findViewById(R.id.eight).setOnClickListener { appendNumber(8) }
+    layout.findViewById(R.id.nine).setOnClickListener { appendNumber(9) }
+    layout.findViewById(R.id.zero).setOnClickListener { appendNumber(0) }
+    layout.findViewById(R.id.delete).setOnClickListener {
+      selectedMinutes /= 10
+      updateTimeState()
+    }
 
     val bookId = arguments.getLong(NI_BOOK_ID)
     val book = repo.bookById(bookId)
@@ -54,15 +104,21 @@ class SleepTimerDialogFragment : DialogFragment() {
       return super.onCreateDialog(savedInstanceState)
     }
 
-    // setup seekBar
-    val min = 5
-    val max = 120
-    seekBar.max = (max - min) * SEEK_FACTOR
-    seekBar.progress = (prefs.sleepTime.value() - min) * SEEK_FACTOR
-    seekBar.onProgressChanged(initialNotification = true) {
-      val corrected = it / SEEK_FACTOR + min
-      val text = resources.getQuantityString(R.plurals.pauses_after, corrected, corrected)
-      textView.text = text
+    fab.setOnClickListener {
+      // should be hidden if
+      check(selectedMinutes > 0) { "fab should be hidden when time is invalid" }
+      prefs.sleepTime.set(selectedMinutes)
+
+      prefs.bookmarkOnSleepTimer.set(bookmarkSwitch.isChecked)
+      if (prefs.bookmarkOnSleepTimer.value()) {
+        val date = DateUtils.formatDateTime(context, System.currentTimeMillis(), DateUtils.FORMAT_SHOW_DATE or DateUtils.FORMAT_SHOW_TIME or DateUtils.FORMAT_NUMERIC_DATE)
+        bookmarkProvider.addBookmarkAtBookPosition(book, date + ": " + getString(R.string.action_sleep))
+      }
+
+      prefs.shakeToReset.set(shakeToResetSwitch.isChecked)
+
+      sandMan.setActive(true)
+      dismiss()
     }
 
     // setup bookmark toggle
@@ -75,31 +131,19 @@ class SleepTimerDialogFragment : DialogFragment() {
       shakeToResetSwitch.visible = false
     }
 
-    return MaterialDialog.Builder(context)
-      .title(R.string.action_sleep)
-      .positiveText(R.string.dialog_confirm)
-      .negativeText(R.string.dialog_cancel)
-      .customView(layout, true)
-      .positiveClicked {
-        val corrected = seekBar.progress / SEEK_FACTOR + min
-        prefs.sleepTime.set(corrected)
-
-        prefs.bookmarkOnSleepTimer.set(bookmarkSwitch.isChecked)
-        if (prefs.bookmarkOnSleepTimer.value()) {
-          val date = DateUtils.formatDateTime(context, System.currentTimeMillis(), DateUtils.FORMAT_SHOW_DATE or DateUtils.FORMAT_SHOW_TIME or DateUtils.FORMAT_NUMERIC_DATE)
-          bookmarkProvider.addBookmarkAtBookPosition(book, date + ": " + getString(R.string.action_sleep))
-        }
-
-        prefs.shakeToReset.set(shakeToResetSwitch.isChecked)
-
-        sandMan.setActive(true)
+    return BottomSheetDialog(context, R.style.BottomSheetStyle).apply {
+      setContentView(layout)
+      // hide the background so the fab looks overlapping
+      setOnShowListener {
+        val parentView = layout.parent as View
+        parentView.background = null
       }
-      .build()
+    }
   }
 
   companion object {
-    private val NI_BOOK_ID = "BOOK_ID"
-    private val SEEK_FACTOR = 10
+    private val NI_BOOK_ID = "ni#bookId"
+    private val SI_MINUTES = "si#time"
     fun newInstance(book: Book) = SleepTimerDialogFragment().apply {
       arguments = Bundle().apply {
         putLong(NI_BOOK_ID, book.id)
