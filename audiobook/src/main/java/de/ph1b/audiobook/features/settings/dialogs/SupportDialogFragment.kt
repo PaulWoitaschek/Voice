eb033b96f3eacd6bd46a64b0af92a20fe82bd65d
Paul Woitaschek <woitaschek@posteo.de>
2017-03-12 20:45:20 +0100

    Catch ActivityNotFoundException in case no browser is installed

--- audiobook/src/main/java/de/ph1b/audiobook/features/settings/dialogs/SupportDialogFragment.kt
+++ audiobook/src/main/java/de/ph1b/audiobook/features/settings/dialogs/SupportDialogFragment.kt
@@ -2,6 +2,7 @@ package de.ph1b.audiobook.features.settings.dialogs
 
 
 import android.app.Dialog
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -10,6 +11,7 @@ import android.support.v4.app.DialogFragment
 import com.afollestad.materialdialogs.MaterialDialog
 
 import de.ph1b.audiobook.R
+import e
 
 
 class SupportDialogFragment : DialogFragment() {
@@ -17,25 +19,30 @@ class SupportDialogFragment : DialogFragment() {
   override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
     val onSupportListItemClicked = MaterialDialog.ListCallback { _, _, i, _ ->
       when (i) {
-      //dev and support
-        0 -> startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(GITHUB_URL)))
-      //translations
-        1 -> startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(TRANSLATION_URL)))
+        0 -> visitUri(GITHUB_URL)
+        1 -> visitUri(TRANSLATION_URL)
         else -> throw AssertionError("There are just 3 items")
       }
     }
 
     return MaterialDialog.Builder(activity)
-      .title(R.string.pref_support_title)
-      .items(R.array.pref_support_values)
-      .itemsCallback(onSupportListItemClicked)
-      .build()
+        .title(R.string.pref_support_title)
+        .items(R.array.pref_support_values)
+        .itemsCallback(onSupportListItemClicked)
+        .build()
   }
 
-  private val GITHUB_URL = "https://github.com/Ph1b/MaterialAudiobookPlayer"
-  private val TRANSLATION_URL = "https://www.transifex.com/projects/p/material-audiobook-player"
+  private fun visitUri(uri: Uri) {
+    try {
+      startActivity(Intent(Intent.ACTION_VIEW, uri))
+    } catch (exception: ActivityNotFoundException) {
+      e(exception)
+    }
+  }
 
   companion object {
     val TAG: String = SupportDialogFragment::class.java.simpleName
+    private val GITHUB_URL = Uri.parse("https://github.com/Ph1b/MaterialAudiobookPlayer")!!
+    private val TRANSLATION_URL = Uri.parse("https://www.transifex.com/projects/p/material-audiobook-player")!!
   }
 }
