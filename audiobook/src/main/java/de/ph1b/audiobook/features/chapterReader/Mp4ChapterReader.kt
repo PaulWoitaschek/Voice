340dc149a5ed22de5e0f2aaaca9a480bebc48bb0
Paul Woitaschek <woitaschek@gmail.com>
2017-05-23 22:59:54 +0200

    Catch IOExceptions while parsing mp4 and report them to crashlytics

--- audiobook/src/main/java/de/ph1b/audiobook/features/chapterReader/Mp4ChapterReader.kt
+++ audiobook/src/main/java/de/ph1b/audiobook/features/chapterReader/Mp4ChapterReader.kt
@@ -1,8 +1,11 @@
 package de.ph1b.audiobook.features.chapterReader
 
 import android.util.SparseArray
+import de.ph1b.audiobook.features.crashlytics.CrashlyticsProxy
+import de.ph1b.audiobook.misc.emptySparseArray
 import de.ph1b.audiobook.misc.isNotEmpty
 import java.io.File
+import java.io.IOException
 
 /**
  * Reads mp4 chapters
@@ -12,9 +15,20 @@ import java.io.File
 object Mp4ChapterReader {
 
   fun readChapters(file: File): SparseArray<String> {
-    val fromChap = ChapReader.read(file)
+    val fromChap = try {
+      ChapReader.read(file)
+    } catch (e: IOException) {
+      CrashlyticsProxy.logException(IOException("Error while parsing $file", e))
+      emptySparseArray<String>()
+    }
+
     if (fromChap.isNotEmpty()) return fromChap
 
-    return ChplReader.read(file)
+    return try {
+      ChplReader.read(file)
+    } catch (e: IOException) {
+      CrashlyticsProxy.logException(IOException("Error while parsing $file", e))
+      emptySparseArray<String>()
+    }
   }
 }
