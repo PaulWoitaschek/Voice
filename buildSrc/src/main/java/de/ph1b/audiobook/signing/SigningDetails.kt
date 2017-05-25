7d7164e04357c43d4da3d868a879f06d98089cdd
Paul Woitaschek <woitaschek@gmail.com>
2017-05-25 10:16:02 +0200

    Handle the signing details in a separate kotlin class

---
+++ buildSrc/src/main/java/de/ph1b/audiobook/signing/SigningDetails.kt
@@ -0,0 +1,6 @@
+package de.ph1b.audiobook.signing
+
+import java.io.File
+
+
+data class SigningDetails(val storeFile: File, val storePassword: String, val keyAlias: String, val keyPassword: String)
---
+++ buildSrc/src/main/java/de/ph1b/audiobook/signing/SigningDetailsProvider.kt
@@ -0,0 +1,35 @@
+package de.ph1b.audiobook.signing
+
+import java.io.File
+import java.util.*
+
+/**
+ * Read the signing details from a properties file.
+ *
+ * @author Paul Woitaschek
+ */
+@Suppress("unused")
+class SigningDetailsProvider {
+
+  private val STORE_FILE = "STORE_FILE"
+  private val STORE_PASSWORD = "STORE_PASSWORD"
+  private val KEY_ALIAS = "KEY_ALIAS"
+  private val KEY_PASSWORD = "KEY_PASSWORD"
+
+  fun provide(propFile: File): SigningDetails? {
+    val props = Properties()
+    if (propFile.canRead()) {
+      props.load(propFile.inputStream())
+      if (props.containsKey(STORE_FILE) && props.containsKey(STORE_PASSWORD) && props.containsKey(KEY_ALIAS) && props.containsKey(KEY_PASSWORD)) {
+        val file = File(props[STORE_FILE].toString())
+        val storePass = props[STORE_PASSWORD].toString()
+        val keyAlias = props[KEY_ALIAS].toString()
+        val keyPass = props[KEY_PASSWORD].toString()
+        return SigningDetails(file, storePass, keyAlias, keyPass)
+      } else throw IllegalArgumentException("Invalid properties $props")
+    } else {
+      println("signing.properties not found")
+      return null
+    }
+  }
+}
