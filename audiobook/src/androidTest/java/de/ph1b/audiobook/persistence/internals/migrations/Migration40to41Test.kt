1e15fe22ec0d8d002ea265371fc43d82871d4859
Paul Woitaschek <woitaschek@gmail.com>
2017-05-31 09:30:20 +0200

    Test the 40->41 migration

---
+++ audiobook/src/androidTest/java/de/ph1b/audiobook/persistence/internals/migrations/Migration40to41Test.kt
@@ -0,0 +1,117 @@
+package de.ph1b.audiobook.persistence.internals.migrations
+
+import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteException
+import android.database.sqlite.SQLiteOpenHelper
+import android.support.test.InstrumentationRegistry
+import de.ph1b.audiobook.persistence.internals.int
+import de.ph1b.audiobook.persistence.internals.mapRows
+import de.ph1b.audiobook.persistence.internals.query
+import de.ph1b.audiobook.persistence.internals.update
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Test the migration from 40 to 41
+
+ * @author Paul Woitaschek
+ */
+class Migration40to41Test {
+
+  private lateinit var db: SQLiteDatabase
+  private lateinit var dbHelper: SQLiteOpenHelper
+
+  @Before
+  fun setUp() {
+    val context = InstrumentationRegistry.getTargetContext()
+    dbHelper = DBHelper(context)
+    context.deleteDatabase(dbHelper.databaseName)
+    db = dbHelper.writableDatabase
+  }
+
+  @Test(expected = SQLiteException::class)
+  fun testInsertBeforeMigrationThrows() {
+    val bookCv = bookContentValues()
+    bookCv.put("loudnessGain", 100)
+    db.insertOrThrow(BookTable.TABLE_NAME, null, bookCv)
+  }
+
+  @Test
+  fun testInsert() {
+    val bookCv = bookContentValues()
+    val id = db.insertOrThrow(BookTable.TABLE_NAME, null, bookCv)
+
+    Migration40to41().migrate(db)
+
+    val loudnessGainCv = ContentValues().apply {
+      put("loudnessGain", 100)
+    }
+    db.update(BookTable.TABLE_NAME, loudnessGainCv, "${BookTable.ID}=?", id)
+
+    val loudnessGains = db.query(BookTable.TABLE_NAME, listOf("loudnessGain")).mapRows {
+      int("loudnessGain")
+    }
+    assertThat(loudnessGains).containsExactly(100)
+  }
+
+  @SuppressLint("SdCardPath")
+  private fun bookContentValues() = ContentValues().apply {
+    put(BookTable.NAME, "firstBookName")
+    put(BookTable.CURRENT_MEDIA_PATH, "/sdcard/file1.mp3")
+    put(BookTable.PLAYBACK_SPEED, 1F)
+    put(BookTable.ROOT, "/sdcard")
+    put(BookTable.TIME, 500)
+    put(BookTable.TYPE, "COLLECTION_FOLDER")
+  }
+
+  @After
+  fun tearDown() {
+    val context = InstrumentationRegistry.getTargetContext()
+    context.deleteDatabase(dbHelper.databaseName)
+  }
+
+
+  class DBHelper(context: Context) : SQLiteOpenHelper(context, "testDb", null, 1) {
+
+    override fun onCreate(db: SQLiteDatabase) {
+      BookTable.onCreate(db)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {}
+  }
+
+  object BookTable {
+    const val ID = "bookId"
+    const val NAME = "bookName"
+    const val AUTHOR = "bookAuthor"
+    const val CURRENT_MEDIA_PATH = "bookCurrentMediaPath"
+    const val PLAYBACK_SPEED = "bookSpeed"
+    const val ROOT = "bookRoot"
+    const val TIME = "bookTime"
+    const val TYPE = "bookType"
+    const val ACTIVE = "BOOK_ACTIVE"
+    const val TABLE_NAME = "tableBooks"
+    private const val CREATE_TABLE = """
+      CREATE TABLE $TABLE_NAME (
+        $ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        $NAME TEXT NOT NULL,
+        $AUTHOR TEXT,
+        $CURRENT_MEDIA_PATH TEXT NOT NULL,
+        $PLAYBACK_SPEED REAL NOT NULL,
+        $ROOT TEXT NOT NULL,
+        $TIME INTEGER NOT NULL,
+        $TYPE TEXT NOT NULL,
+        $ACTIVE INTEGER NOT NULL DEFAULT 1
+      )
+    """
+
+    fun onCreate(db: SQLiteDatabase) {
+      db.execSQL(CREATE_TABLE)
+    }
+  }
+}
