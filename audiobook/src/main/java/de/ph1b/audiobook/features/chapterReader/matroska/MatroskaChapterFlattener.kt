88959ab846a06f209922a62cde2c203e8275984c
Paul Woitaschek <woitaschek@gmail.com>
2017-07-19 22:28:50 +0200

    Refactored the chapter flattener and remove the inner function

--- audiobook/src/main/java/de/ph1b/audiobook/features/chapterReader/matroska/MatroskaChapterFlattener.kt
+++ audiobook/src/main/java/de/ph1b/audiobook/features/chapterReader/matroska/MatroskaChapterFlattener.kt
@@ -5,23 +5,26 @@ import android.util.SparseArray
 
 object MatroskaChapterFlattener {
 
+  private lateinit var target: SparseArray<String>
+  private lateinit var preferredLanguages: List<String>
+
+  @Synchronized
   fun toSparseArray(list: List<MatroskaChapter>, preferredLanguages: List<String>): SparseArray<String> {
-    val res = SparseArray<String>()
-
-    fun addChapter(chapters: List<MatroskaChapter>, depth: Int) {
-      chapters.forEachIndexed { i, chapter ->
-        val duration = (chapter.startTime / 1000000).toInt() +
-            if (i == 0) depth else 0
-        // Simple hack with adding depth is needed because chapter
-        // and it's first sub-chapter have usually the same starting time.
-        val name = "+ ".repeat(depth) + (chapter.getName(preferredLanguages) ?: "Chapter ${i + 1}")
-        res.put(duration, name)
-        addChapter(chapter.children, depth + 1)
-      }
-    }
-
+    target = SparseArray()
+    this.preferredLanguages = preferredLanguages
     addChapter(list, 0)
+    return target
+  }
 
-    return res
+  private fun addChapter(chapters: List<MatroskaChapter>, depth: Int) {
+    chapters.forEachIndexed { i, chapter ->
+      val duration = (chapter.startTime / 1000000).toInt() +
+          if (i == 0) depth else 0
+      // Simple hack with adding depth is needed because chapter
+      // and it's first sub-chapter have usually the same starting time.
+      val name = "+ ".repeat(depth) + (chapter.getName(preferredLanguages) ?: "Chapter ${i + 1}")
+      target.put(duration, name)
+      addChapter(chapter.children, depth + 1)
+    }
   }
 }
