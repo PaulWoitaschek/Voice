e66ceef1a26d19b9db1b6936fb26c8f4214885fc
Paul Woitaschek <woitaschek@gmail.com>
2017-07-19 22:17:15 +0200

    Simplified the mapping for matroska chapters

--- audiobook/src/main/java/de/ph1b/audiobook/features/chapterReader/matroska/MatroskaChapter.kt
+++ audiobook/src/main/java/de/ph1b/audiobook/features/chapterReader/matroska/MatroskaChapter.kt
@@ -3,12 +3,14 @@ package de.ph1b.audiobook.features.chapterReader.matroska
 data class MatroskaChapter(
     val startTime: Long,
     val names: List<MatroskaChapterName>,
-    val children: List<MatroskaChapter>) {
-  fun getName(vararg preferredLanguages: String): String?
-      = preferredLanguages
-      .map { language ->
-        names.find { language in it.languages }?.name
+    val children: List<MatroskaChapter>
+) {
+
+  fun getName(vararg preferredLanguages: String): String? = preferredLanguages
+      .mapNotNull { language ->
+        names.find { language in it.languages }
+            ?.name
       }
-      .filterNotNull()
-      .firstOrNull() ?: names.firstOrNull()?.name
+      .firstOrNull()
+      ?: names.firstOrNull()?.name
 }
