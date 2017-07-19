1a953247f1316963b540d82c1b597ec6b2e1f9be
Paul Woitaschek <woitaschek@gmail.com>
2017-07-19 23:04:33 +0200

    Refactored the chapter test into two methods as they are testing different things.

--- audiobook/src/test/java/de/ph1b/audiobook/features/chapterReader/matroska/MatroskaChapterTest.kt
+++ audiobook/src/test/java/de/ph1b/audiobook/features/chapterReader/matroska/MatroskaChapterTest.kt
@@ -8,7 +8,6 @@ class MatroskaChapterTest {
 
   @Test
   fun testGetName() {
-    assertThat(MatroskaChapter(0L, listOf(), listOf()).getName()).isNull()
     val chapter = MatroskaChapter(0L, listOf(
         MatroskaChapterName("Podczęść 1", setOf("pol")),
         MatroskaChapterName("Subpart 1", setOf("eng", "ger")),
@@ -17,5 +16,11 @@ class MatroskaChapterTest {
     assertThat(chapter.getName()).isEqualTo("Podczęść 1")
     assertThat(chapter.getName(listOf("ger", "jpn"))).isEqualTo("Subpart 1")
     assertThat(chapter.getName(listOf("ind", "kac", "jpn", "eng"))).isEqualTo("サブパート1")
+  }
+
+  @Test
+  fun testNoContentsLeadsToNull() {
+    val actual = MatroskaChapter(0L, listOf(), listOf()).getName()
+    assertThat(actual).isNull()
   }
 }
