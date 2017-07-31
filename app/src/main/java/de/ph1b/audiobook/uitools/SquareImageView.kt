12cd949419b804fbf79e36d4a17a468fc2e0ef48
Paul Woitaschek <woitaschek@gmail.com>
2017-07-31 09:11:17 +0200

    Let the SquareImageView extend from AppCompatImageView.

--- app/src/main/java/de/ph1b/audiobook/uitools/SquareImageView.kt
+++ app/src/main/java/de/ph1b/audiobook/uitools/SquareImageView.kt
@@ -1,14 +1,12 @@
 package de.ph1b.audiobook.uitools
 
 import android.content.Context
+import android.support.v7.widget.AppCompatImageView
 import android.util.AttributeSet
-import android.widget.ImageView
 
-class SquareImageView : ImageView {
+class SquareImageView : AppCompatImageView {
   constructor(context: Context) : super(context)
-
   constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
-
   constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
   override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
