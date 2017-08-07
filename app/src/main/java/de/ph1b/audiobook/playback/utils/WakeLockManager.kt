b4c7145372a5c619eadb059f54b40abdcc1a94bd
Paul Woitaschek <woitaschek@gmail.com>
2017-08-07 23:01:22 +0200

    Suppress the wakelock timeout warning.

--- app/src/main/java/de/ph1b/audiobook/playback/utils/WakeLockManager.kt
+++ app/src/main/java/de/ph1b/audiobook/playback/utils/WakeLockManager.kt
@@ -1,5 +1,6 @@
 package de.ph1b.audiobook.playback.utils
 
+import android.annotation.SuppressLint
 import android.os.PowerManager
 import de.ph1b.audiobook.BuildConfig
 import javax.inject.Inject
@@ -12,6 +13,7 @@ class WakeLockManager @Inject constructor(powerManager: PowerManager) {
   private val lock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK or PowerManager.ON_AFTER_RELEASE, BuildConfig.APPLICATION_ID)
       .apply { setReferenceCounted(false) }
 
+  @SuppressLint("WakelockTimeout") // audiobooks are potentially very long
   fun stayAwake(stayAwake: Boolean) {
     if (stayAwake && !lock.isHeld) {
       lock.acquire()
