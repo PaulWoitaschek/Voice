dfd32af129cb5fcbe270c5a0c573f16b7e6e75f9
Paul Woitaschek <woitaschek@posteo.de>
2017-10-06 21:52:19 +0200

    Don't cache the chapter component.

--- chapterreader/src/main/java/de/ph1b/audiobook/chapterreader/ChapterReaderFactory.kt
+++ chapterreader/src/main/java/de/ph1b/audiobook/chapterreader/ChapterReaderFactory.kt
@@ -1,21 +1,15 @@
 package de.ph1b.audiobook.chapterreader
 
-import de.ph1b.audiobook.chapterreader.di.ChapterReaderComponent
 import de.ph1b.audiobook.chapterreader.di.DaggerChapterReaderComponent
 import de.ph1b.audiobook.common.ErrorReporter
 import de.ph1b.audiobook.common.Logger
 
 object ChapterReaderFactory {
 
-  private var component: ChapterReaderComponent? = null
-
-  fun create(logger: Logger, errorReporter: ErrorReporter): ChapterReader {
-    if (component == null) {
-      component = DaggerChapterReaderComponent.builder()
+  fun create(logger: Logger, errorReporter: ErrorReporter): ChapterReader =
+      DaggerChapterReaderComponent.builder()
           .logger(logger)
           .errorReporter(errorReporter)
           .build()
-    }
-    return component!!.chapterReader()
-  }
+          .chapterReader()
 }
