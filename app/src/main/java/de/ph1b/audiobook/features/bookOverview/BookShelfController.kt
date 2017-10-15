7325a3393de2bfcfcc8b0cd2487a285fb9565085
Paul Woitaschek <woitaschek@gmail.com>
2017-10-15 11:15:12 +0200

    Dependency update.

--- app/src/main/java/de/ph1b/audiobook/features/bookOverview/BookShelfController.kt
+++ app/src/main/java/de/ph1b/audiobook/features/bookOverview/BookShelfController.kt
@@ -41,23 +41,6 @@ import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Named
 
-interface BookShelfView {
-  /** Display a new set of books */
-  fun displayNewBooks(books: List<Book>)
-
-  /** The book marked as current was changed. Updates the adapter and fab accordingly. */
-  fun updateCurrentBook(currentBook: Book?)
-
-  /** Sets the fab icon correctly accordingly to the new play state. */
-  fun showPlaying(playing: Boolean)
-
-  /** Show a warning that no audiobook folder was chosen */
-  fun showNoFolderWarning()
-
-  fun showLoading(loading: Boolean)
-  fun bookCoverChanged(bookId: Long)
-}
-
 private const val COVER_FROM_GALLERY = 1
 
 /**
