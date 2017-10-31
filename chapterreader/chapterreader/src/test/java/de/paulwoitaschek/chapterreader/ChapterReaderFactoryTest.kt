85e0c252461cb2f1ed03cfa6346b9901044722a8
Paul Woitaschek <woitaschek@posteo.de>
2017-10-31 22:46:25 +0100

    Added a unit test for the factory.

---
+++ chapterreader/chapterreader/src/test/java/de/paulwoitaschek/chapterreader/ChapterReaderFactoryTest.kt
@@ -0,0 +1,12 @@
+package de.paulwoitaschek.chapterreader
+
+import org.junit.Test
+
+class ChapterReaderFactoryTest {
+
+  @Test
+  fun create() {
+    // just ensure it creates
+    ChapterReaderFactory.create(NoOpLogger)
+  }
+}
