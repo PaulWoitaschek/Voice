259b43688a7403ac987c81942ff5443ac6a0ee48
Paul Woitaschek <woitaschek@posteo.de>
2017-10-31 23:28:48 +0100

    Made the extensions internal and added a unit test.

--- chapterreader/chapterreader/src/main/java/de/paulwoitaschek/chapterreader/misc/binaryExtensions.kt
+++ chapterreader/chapterreader/src/main/java/de/paulwoitaschek/chapterreader/misc/binaryExtensions.kt
@@ -3,9 +3,9 @@ package de.paulwoitaschek.chapterreader.misc
 import java.io.EOFException
 import java.io.InputStream
 
-fun InputStream.skipBytes(number: Int) = IOUtils.skipFully(this, number.toLong())
+internal fun InputStream.skipBytes(number: Int) = IOUtils.skipFully(this, number.toLong())
 
-fun InputStream.readAmountOfBytes(number: Int): ByteArray {
+internal fun InputStream.readAmountOfBytes(number: Int): ByteArray {
   val buf = ByteArray(number)
   var index = 0
   while (index < number) {
@@ -16,13 +16,13 @@ fun InputStream.readAmountOfBytes(number: Int): ByteArray {
   return buf
 }
 
-fun InputStream.readUInt8(): Int {
+internal fun InputStream.readUInt8(): Int {
   val res = read()
   if (res == -1) throw EOFException()
   return res
 }
 
-fun InputStream.readLeUInt32(): Long {
+internal fun InputStream.readLeUInt32(): Long {
   val buf = readAmountOfBytes(4)
   return buf[0].toULong() or
     (buf[1].toULong() shl 8) or
@@ -30,7 +30,7 @@ fun InputStream.readLeUInt32(): Long {
     (buf[3].toULong() shl 24)
 }
 
-fun InputStream.readLeInt32(): Int {
+internal fun InputStream.readLeInt32(): Int {
   val buf = readAmountOfBytes(4)
   return buf[0].toUInt() or
     (buf[1].toUInt() shl 8) or
@@ -38,7 +38,7 @@ fun InputStream.readLeInt32(): Int {
     (buf[3].toUInt() shl 24)
 }
 
-fun InputStream.readLeInt64(): Long {
+internal fun InputStream.readLeInt64(): Long {
   val buf = readAmountOfBytes(8)
   return buf[0].toULong() or
     (buf[1].toULong() shl 8) or
@@ -50,11 +50,11 @@ fun InputStream.readLeInt64(): Long {
     (buf[7].toULong() shl 56)
 }
 
-fun ByteArray.startsWith(prefix: ByteArray): Boolean {
+internal fun ByteArray.startsWith(prefix: ByteArray): Boolean {
   if (this.size < prefix.size) return false
   return prefix.withIndex().all { (i, v) -> v == this[i] }
 }
 
-fun Byte.toUInt(): Int = toInt() and 0xFF
-fun Byte.toULong() = toLong() and 0xFFL
+internal fun Byte.toUInt(): Int = toInt() and 0xFF
+internal fun Byte.toULong() = toLong() and 0xFFL
 
