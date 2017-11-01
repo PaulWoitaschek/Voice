d247dbbc5b913a475345f5b6e3ff7339bcad88ea
Paul Woitaschek <woitaschek@posteo.de>
2017-11-01 08:36:54 +0100

    Use truths containsExactly instead of manually creating and comparing to a list.

--- chapterreader/chapterreader/src/test/java/de/paulwoitaschek/chapterreader/ogg/OggReadingTest.kt
+++ chapterreader/chapterreader/src/test/java/de/paulwoitaschek/chapterreader/ogg/OggReadingTest.kt
@@ -35,16 +35,14 @@ class OggReadingTest {
           255, 255, 14, 255, 0, 255, 255, 17
         )
       )
-    )
-      .isEqualTo(listOf(2 * 255 + 14, 255, 2 * 255 + 17))
+    ).containsExactly(2 * 255 + 14, 255, 2 * 255 + 17)
     assertThat(
       PackageSizeParser.fromSegmentTable(
         unsignedByteArrayOf(
           255, 255, 255, 255, 70, 255, 255
         )
       )
-    )
-      .isEqualTo(listOf(4 * 255 + 70, 2 * 255))
+    ).containsExactly(4 * 255 + 70, 2 * 255)
   }
 
   @Test
@@ -54,44 +52,42 @@ class OggReadingTest {
       readOggPages(it).toList()
     }
 
-    assertThat(pages).isEqualTo(
-      listOf(
-        OggPage(
-          continuedPacket = false,
-          finishedPacket = true,
-          firstPageOfStream = true,
-          lastPageOfStream = false,
-          absoluteGranulePosition = 0,
-          streamSerialNumber = 7891011,
-          pageSequenceNumber = 0,
-          packets = listOf(
-            "asdeceq".toByteArray()
-          )
-        ),
-        OggPage(
-          continuedPacket = false,
-          finishedPacket = false,
-          firstPageOfStream = false,
-          lastPageOfStream = false,
-          absoluteGranulePosition = -1,
-          streamSerialNumber = 7891011,
-          pageSequenceNumber = 1,
-          packets = listOf(
-            "b".repeat(255).toByteArray()
-          )
-        ),
-        OggPage(
-          continuedPacket = true,
-          finishedPacket = true,
-          firstPageOfStream = false,
-          lastPageOfStream = true,
-          absoluteGranulePosition = 123459,
-          streamSerialNumber = 7891011,
-          pageSequenceNumber = 2,
-          packets = listOf(
-            "bbb".toByteArray(),
-            "ae".toByteArray()
-          )
+    assertThat(pages).containsExactly(
+      OggPage(
+        continuedPacket = false,
+        finishedPacket = true,
+        firstPageOfStream = true,
+        lastPageOfStream = false,
+        absoluteGranulePosition = 0,
+        streamSerialNumber = 7891011,
+        pageSequenceNumber = 0,
+        packets = listOf(
+          "asdeceq".toByteArray()
+        )
+      ),
+      OggPage(
+        continuedPacket = false,
+        finishedPacket = false,
+        firstPageOfStream = false,
+        lastPageOfStream = false,
+        absoluteGranulePosition = -1,
+        streamSerialNumber = 7891011,
+        pageSequenceNumber = 1,
+        packets = listOf(
+          "b".repeat(255).toByteArray()
+        )
+      ),
+      OggPage(
+        continuedPacket = true,
+        finishedPacket = true,
+        firstPageOfStream = false,
+        lastPageOfStream = true,
+        absoluteGranulePosition = 123459,
+        streamSerialNumber = 7891011,
+        pageSequenceNumber = 2,
+        packets = listOf(
+          "bbb".toByteArray(),
+          "ae".toByteArray()
         )
       )
     )
