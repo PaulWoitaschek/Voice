c7d8c2027e3b3ffae27a735c9aec4c01cdb1fea8
Paul Woitaschek <woitaschek@posteo.de>
2018-10-15 10:31:39 +0200

    Refactored the fastlane logic to a gradle task.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import com.android.build.gradle.AppPlugin
 import com.android.build.gradle.BaseExtension
 import com.android.build.gradle.LibraryPlugin
+import com.android.build.gradle.tasks.LintBaseTask
 import com.android.builder.model.AndroidProject
 import com.github.benmanes.gradle.versions.updates.DependencyUpdates
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
@@ -29,14 +30,9 @@ plugins {
   id("com.github.ben-manes.versions") version "0.20.0"
 }
 
-
 buildScan {
   setTermsOfServiceUrl("https://gradle.com/terms-of-service")
   setTermsOfServiceAgree("yes")
-}
-
-tasks.register("appVersion") {
-  print("#BEGIN_VERSION#${Versions.versionName}_${Versions.versionCode}#END_VERSION#")
 }
 
 tasks {
@@ -82,13 +78,59 @@ subprojects {
   plugins.whenPluginAdded {
     if (this is AppPlugin || this is LibraryPlugin) {
       convention.findByType(BaseExtension::class)?.let {
-        it.dexOptions.preDexLibraries = System.getenv("DISABLE_PREDEX") != "true"
+        it.dexOptions.preDexLibraries = System.getenv("CI") != "true"
       }
     }
   }
 }
 
-tasks.register<Exec>("importStrings") {
-  executable = "sh"
-  args("-c", "tx pull -af --minimum-perc=5")
+tasks {
+
+  register<Exec>("importStrings") {
+    executable = "sh"
+    args("-c", "tx pull -af --minimum-perc=5")
+  }
+
+  val artifactFolder = File(
+    "${rootDir.absolutePath}/artifacts/${Versions.versionName}_${Versions.versionCode}"
+  )
+
+  val ciBuildApks = register<GradleBuild>("ciBuildApks") {
+    val assembleTask = subprojects.single { it.name == "app" }
+      .tasks.single { it.name == "assembleProprietaryRelease" }.also { task ->
+      task.dependsOn.removeAll {
+        val name = it.toString()
+        name.contains("lint") || name.contains("crashlytics")
+      }
+    }
+    dependsOn(assembleTask)
+  }
+
+  val copyCiApk = register<Copy>("copyCiApk") {
+    from("app/build/outputs/apk/proprietary/release") {
+      include("*.apk")
+    }
+    into(artifactFolder)
+  }
+
+  val allUnitTests = register<GradleBuild>("allUnitTests") {
+    val testResultFolder = File(rootDir, "testResults")
+    val tests = subprojects.mapNotNull { subProject ->
+      val test = (subProject.tasks.findByName("testProprietaryDebugUnitTest")
+        ?: subProject.tasks.findByName("testDebugUnitTest")) as? Test
+      if (test != null) {
+        test.reports.junitXml.destination = File(testResultFolder, subProject.name)
+      }
+      test
+    }
+    dependsOn(tests)
+  }
+
+  register<GradleBuild>("ci") {
+    val lint =
+      subprojects.single { it.name == "app" }.tasks
+        .single { it.name == "lintProprietaryDebug" } as LintBaseTask
+    lint.lintOptions.htmlOutput = File(artifactFolder, "lint.html")
+    dependsOn(ciBuildApks, copyCiApk.get().mustRunAfter(ciBuildApks), allUnitTests, lint)
+  }
 }
