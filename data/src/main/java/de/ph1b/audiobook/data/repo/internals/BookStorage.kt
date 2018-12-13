b6c27f098991f601d470e21461944879e206cb81
Paul Woitaschek <woitaschek@posteo.de>
2018-12-13 09:49:05 +0100

    Run the book storage access on a single thread dispatcher.

--- data/src/main/java/de/ph1b/audiobook/data/repo/internals/BookStorage.kt
+++ data/src/main/java/de/ph1b/audiobook/data/repo/internals/BookStorage.kt
@@ -3,11 +3,10 @@ package de.ph1b.audiobook.data.repo.internals
 import androidx.room.RoomDatabase
 import de.ph1b.audiobook.data.Book
 import de.ph1b.audiobook.data.BookContent
-import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.withContext
 import java.util.UUID
+import java.util.concurrent.Executors
 import javax.inject.Inject
 
 /**
@@ -21,62 +20,69 @@ class BookStorage
   private val appDb: AppDb
 ) {
 
-  suspend fun books(): List<Book> = synchronizedWithIoDispatcher {
-    appDb.transaction {
-      bookSettingsDao.all()
-        .map { bookSettings ->
-          val bookId = bookSettings.id
-          val metaData = metaDataDao.byId(bookId)
-          val chapters = chapterDao.byBookId(bookId)
+  suspend fun books(): List<Book> {
+    return synchronizedWithIoDispatcher {
+      appDb.transaction {
+        bookSettingsDao.all()
+          .map { bookSettings ->
+            val bookId = bookSettings.id
+            val metaData = metaDataDao.byId(bookId)
+            val chapters = chapterDao.byBookId(bookId)
 
-          Book(
-            id = bookId, content = BookContent(
-              id = bookId,
-              chapters = chapters,
-              settings = bookSettings
-            ),
-            metaData = metaData
-          )
-        }
+            Book(
+              id = bookId, content = BookContent(
+                id = bookId,
+                chapters = chapters,
+                settings = bookSettings
+              ),
+              metaData = metaData
+            )
+          }
+      }
     }
   }
 
-  suspend fun revealBook(bookId: UUID) = synchronizedWithIoDispatcher {
-    bookSettingsDao.setActive(bookId, true)
+  suspend fun revealBook(bookId: UUID) {
+    synchronizedWithIoDispatcher {
+      bookSettingsDao.setActive(bookId, true)
+    }
   }
 
-  suspend fun hideBook(bookId: UUID) = synchronizedWithIoDispatcher {
-    bookSettingsDao.setActive(bookId, false)
+  suspend fun hideBook(bookId: UUID) {
+    synchronizedWithIoDispatcher {
+      bookSettingsDao.setActive(bookId, false)
+    }
   }
 
-  suspend fun addOrUpdate(book: Book) = synchronizedWithIoDispatcher {
-    appDb.transaction {
-      metaDataDao.insert(book.metaData)
-      bookSettingsDao.insert(book.content.settings)
-      // delete old chapters and replace them with new ones
-      chapterDao.deleteByBookId(book.id)
-      chapterDao.insert(book.content.chapters)
+  suspend fun addOrUpdate(book: Book) {
+    synchronizedWithIoDispatcher {
+      appDb.transaction {
+        metaDataDao.insert(book.metaData)
+        bookSettingsDao.insert(book.content.settings)
+        // delete old chapters and replace them with new ones
+        chapterDao.deleteByBookId(book.id)
+        chapterDao.insert(book.content.chapters)
+      }
     }
   }
 
   private suspend inline fun <T> synchronizedWithIoDispatcher(crossinline action: () -> T): T {
-    return mutex.withLock {
-      withContext(IO) {
-        action()
-      }
+    // as the dispatcher is single threaded, we have implicit thread safety.
+    return withContext(STORAGE_DISPATCHER) {
+      action()
     }
   }
 
   private inline fun <T> RoomDatabase.transaction(action: () -> T): T {
     beginTransaction()
     return try {
-      val result = action()
-      setTransactionSuccessful()
-      result
+      action().also {
+        setTransactionSuccessful()
+      }
     } finally {
       endTransaction()
     }
   }
 }
 
-private val mutex = Mutex()
+private val STORAGE_DISPATCHER = Executors.newSingleThreadExecutor().asCoroutineDispatcher()
