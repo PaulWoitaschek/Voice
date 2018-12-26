f63af7fc83c7105aec1a79716a583a66df7abbc6
Paul Woitaschek <woitaschek@posteo.de>
2018-12-26 22:09:49 +0100

    Prepare for 4.0.2

--- buildSrc/src/main/kotlin/deps/Deps.kt
+++ buildSrc/src/main/kotlin/deps/Deps.kt
@@ -10,8 +10,8 @@ object Versions {
   const val compileSdk = 28
   const val minSdk = 21
   const val targetSdk = 27
-  const val versionCode = 3060317
-  const val versionName = "4.0.1"
+  const val versionCode = 3060318
+  const val versionName = "4.0.2"
 }
 
 object Deps {
