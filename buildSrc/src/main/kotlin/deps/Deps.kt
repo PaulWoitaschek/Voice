8f62e626890436ff9185129eb08f9d1072864123
Paul Woitaschek <woitaschek@posteo.de>
2019-04-30 21:30:50 +0200

    Stable android plugin 3.4

--- buildSrc/src/main/kotlin/deps/Deps.kt
+++ buildSrc/src/main/kotlin/deps/Deps.kt
@@ -40,7 +40,7 @@ object Deps {
     }
   }
 
-  const val androidGradlePlugin = "com.android.tools.build:gradle:3.4.0-rc03"
+  const val androidGradlePlugin = "com.android.tools.build:gradle:3.4.0"
   const val material = "com.google.android.material:material:1.0.0"
   const val floatingActionButton = "com.getbase:floatingactionbutton:1.10.1"
   const val materialCab = "com.afollestad:material-cab:2.0.1"
