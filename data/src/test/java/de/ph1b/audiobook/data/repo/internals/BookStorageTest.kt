0b132acba929ec92297818ffa3d0100927c78f62
Paul Woitaschek <woitaschek@posteo.de>
2020-03-25 07:16:52 +0100

    Fixed the BookStorageTest setup by retrieving the books from the db.

--- data/src/test/java/de/ph1b/audiobook/data/repo/internals/BookStorageTest.kt
+++ data/src/test/java/de/ph1b/audiobook/data/repo/internals/BookStorageTest.kt
@@ -4,30 +4,37 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
+import de.ph1b.audiobook.data.Book
 import de.ph1b.audiobook.data.BookFactory
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
+import java.util.UUID
 
 @RunWith(AndroidJUnit4::class)
 @Config(sdk = [28])
 class BookStorageTest {
 
-  private val db =
-    Room.inMemoryDatabaseBuilder(ApplicationProvider.getApplicationContext(), AppDb::class.java)
-      .allowMainThreadQueries()
-      .build()
+  private val db = Room.inMemoryDatabaseBuilder(ApplicationProvider.getApplicationContext(), AppDb::class.java)
+    .allowMainThreadQueries()
+    .build()
   private val storage = BookStorage(db.chapterDao(), db.bookMetadataDao(), db.bookSettingsDao(), db)
 
-  val bookA = BookFactory.create(name = "Name A", lastPlayedAtMillis = 5)
-  val bookB = BookFactory.create(name = "Name B", lastPlayedAtMillis = 10)
+  private val bookA: Book
+  private val bookB: Book
 
   init {
-    runBlocking {
-      storage.addOrUpdate(bookA)
-      storage.addOrUpdate(bookB)
+    val bookAId = UUID.randomUUID()
+    val bookBId = UUID.randomUUID()
+    val books = runBlocking {
+      storage.addOrUpdate(BookFactory.create(name = "Name A", lastPlayedAtMillis = 5, id = bookAId))
+      storage.addOrUpdate(BookFactory.create(name = "Name B", lastPlayedAtMillis = 10, id = bookBId))
+      storage.books()
     }
+
+    bookA = books.single { it.id == bookAId }
+    bookB = books.single { it.id == bookBId }
   }
 
   @Test
