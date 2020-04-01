702d7df0275920c4bbdcd50dafc852bd07d07cb2
Paul Woitaschek <woitaschek@posteo.de>
2020-04-01 22:26:34 +0200

    Prepare for 5.0.0

--- buildSrc/src/main/kotlin/deps/Deps.kt
+++ buildSrc/src/main/kotlin/deps/Deps.kt
@@ -10,8 +10,8 @@ object Versions {
   const val compileSdk = 29
   const val minSdk = 23
   const val targetSdk = 29
-  const val versionCode = 3060339
-  const val versionName = "5.0.0-rc13"
+  const val versionCode = 3060340
+  const val versionName = "5.0.0"
 }
 
 object Deps {
