4affbb11ac9b950dd2e25c4679609f909e2d43db
Paul Woitaschek <woitaschek@posteo.de>
2020-04-03 22:19:26 +0200

    Replaced SwitchCompat with SwitchMaterial. Fixes #960

--- app/src/main/java/de/ph1b/audiobook/features/settings/SwitchSettingView.kt
+++ app/src/main/java/de/ph1b/audiobook/features/settings/SwitchSettingView.kt
@@ -3,24 +3,26 @@ package de.ph1b.audiobook.features.settings
 import android.content.Context
 import android.os.Build
 import android.util.AttributeSet
-import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.res.use
 import androidx.core.view.isVisible
 import androidx.core.view.updatePadding
 import de.ph1b.audiobook.R
+import de.ph1b.audiobook.databinding.MergeSettingRowSwitchBinding
 import de.ph1b.audiobook.misc.dpToPxRounded
+import de.ph1b.audiobook.misc.layoutInflater
 import de.ph1b.audiobook.uitools.drawableFromAttr
-import kotlinx.android.synthetic.main.merge_setting_row_switch.view.*
 
 class SwitchSettingView : ConstraintLayout {
+
+  private val binding = MergeSettingRowSwitchBinding.inflate(layoutInflater(), this)
 
   constructor(context: Context) : super(context)
   constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
     context.theme.obtainStyledAttributes(attrs, R.styleable.SwitchSettingView, 0, 0).use {
-      switchTitle.text = it.getText(R.styleable.SwitchSettingView_ssv_title)
-      switchDescription.text = it.getText(R.styleable.SwitchSettingView_ssv_description)
-      switchDescription.isVisible = switchDescription.text?.isNotBlank() == true
+      binding.switchTitle.text = it.getText(R.styleable.SwitchSettingView_ssv_title)
+      binding.switchDescription.text = it.getText(R.styleable.SwitchSettingView_ssv_description)
+      binding.switchDescription.isVisible = binding.switchDescription.text?.isNotBlank() == true
     }
   }
 
@@ -28,7 +30,6 @@ class SwitchSettingView : ConstraintLayout {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
       foreground = context.drawableFromAttr(R.attr.selectableItemBackground)
     }
-    View.inflate(context, R.layout.merge_setting_row_switch, this)
     val padding = context.dpToPxRounded(8F)
     updatePadding(top = padding, bottom = padding)
   }
@@ -38,6 +39,6 @@ class SwitchSettingView : ConstraintLayout {
   }
 
   fun setChecked(checked: Boolean) {
-    switchSetting.isChecked = checked
+    binding.switchSetting.isChecked = checked
   }
 }
