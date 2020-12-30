e89657c939829daa7ffd5e8f4b91372ecb2bf3ca
Paul Woitaschek <paul@yazio.com>
2020-12-30 21:35:32 +0100

    Directly pass the renderers in a RenderersFactory.

--- app/src/main/java/de/ph1b/audiobook/playback/OnlyAudioRenderersFactory.kt
+++ app/src/main/java/de/ph1b/audiobook/playback/OnlyAudioRenderersFactory.kt
@@ -2,62 +2,35 @@ package de.ph1b.audiobook.playback
 
 import android.content.Context
 import android.os.Handler
-import android.os.Looper
-import com.google.android.exoplayer2.DefaultRenderersFactory
 import com.google.android.exoplayer2.Renderer
+import com.google.android.exoplayer2.RenderersFactory
+import com.google.android.exoplayer2.audio.AudioRendererEventListener
+import com.google.android.exoplayer2.audio.MediaCodecAudioRenderer
 import com.google.android.exoplayer2.drm.DrmSessionManager
 import com.google.android.exoplayer2.drm.FrameworkMediaCrypto
+import com.google.android.exoplayer2.ext.flac.LibflacAudioRenderer
 import com.google.android.exoplayer2.mediacodec.MediaCodecSelector
 import com.google.android.exoplayer2.metadata.MetadataOutput
 import com.google.android.exoplayer2.text.TextOutput
 import com.google.android.exoplayer2.video.VideoRendererEventListener
-import java.util.ArrayList
 import javax.inject.Inject
 
 class OnlyAudioRenderersFactory
 @Inject constructor(
-  context: Context
-) : DefaultRenderersFactory(context) {
+  private val context: Context
+) : RenderersFactory {
 
-  init {
-    setExtensionRendererMode(EXTENSION_RENDERER_MODE_PREFER)
-  }
-
-  override fun buildMetadataRenderers(
-    context: Context,
-    output: MetadataOutput,
-    outputLooper: Looper,
-    extensionRendererMode: Int,
-    out: ArrayList<Renderer>
-  ) {
-  }
-
-  override fun buildMiscellaneousRenderers(context: Context, eventHandler: Handler, extensionRendererMode: Int, out: ArrayList<Renderer>) {
-  }
-
-  override fun buildVideoRenderers(
-    context: Context,
-    extensionRendererMode: Int,
-    mediaCodecSelector: MediaCodecSelector,
-    drmSessionManager: DrmSessionManager<FrameworkMediaCrypto>?,
-    playClearSamplesWithoutKeys: Boolean,
-    enableDecoderFallback: Boolean,
+  override fun createRenderers(
     eventHandler: Handler,
-    eventListener: VideoRendererEventListener,
-    allowedVideoJoiningTimeMs: Long,
-    out: ArrayList<Renderer>
-  ) {
-  }
-
-  override fun buildCameraMotionRenderers(context: Context, extensionRendererMode: Int, out: ArrayList<Renderer>) {
-  }
-
-  override fun buildTextRenderers(
-    context: Context,
-    output: TextOutput,
-    outputLooper: Looper,
-    extensionRendererMode: Int,
-    out: ArrayList<Renderer>
-  ) {
+    videoRendererEventListener: VideoRendererEventListener,
+    audioRendererEventListener: AudioRendererEventListener,
+    textRendererOutput: TextOutput,
+    metadataRendererOutput: MetadataOutput,
+    drmSessionManager: DrmSessionManager<FrameworkMediaCrypto>?
+  ): Array<Renderer> {
+    return arrayOf(
+      LibflacAudioRenderer(eventHandler, audioRendererEventListener),
+      MediaCodecAudioRenderer(context, MediaCodecSelector.DEFAULT, eventHandler, audioRendererEventListener)
+    )
   }
 }
