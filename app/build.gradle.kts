d7eb3a71c0b0249ec742238ea55abb9dda2f8e81
Paul Woitaschek <woitaschek@gmail.com>
2022-02-20 23:50:26 +0100

    Prepare for 6.0.0-alpha12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
 
   defaultConfig {
     applicationId = "de.ph1b.audiobook"
-    versionCode = 3060350
-    versionName = "6.0.0-alpha11"
+    versionCode = 3060351
+    versionName = "6.0.0-alpha12"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
