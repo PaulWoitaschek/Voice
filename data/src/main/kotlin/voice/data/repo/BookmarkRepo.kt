337eef422d32c4002ed43ca2a886e7c505d6343f
Paul Woitaschek <woitaschek@gmail.com>
2022-03-16 08:43:14 +0100

    Split the chapters for the SQLITE_MAX_VARIABLE_NUMBER

--- data/src/main/kotlin/voice/data/repo/BookmarkRepo.kt
+++ data/src/main/kotlin/voice/data/repo/BookmarkRepo.kt
@@ -8,6 +8,7 @@ import voice.data.Bookmark
 import voice.data.repo.internals.AppDb
 import voice.data.repo.internals.dao.BookmarkDao
 import voice.data.repo.internals.transaction
+import voice.data.runForMaxSqlVariableNumber
 import voice.logging.core.Logger
 import java.time.Instant
 import javax.inject.Inject
@@ -45,17 +46,10 @@ class BookmarkRepo
 
   suspend fun bookmarks(book: BookContent): List<Bookmark> {
     val chapters = book.chapters
-    // we can only query SQLITE_MAX_VARIABLE_NUMBER at once (999 bugs on some devices so we use a number a little smaller.)
-    // if it's larger than the limit, we query in chunks.
-    val limit = 990
-    return if (chapters.size > limit) {
-      appDb.transaction {
-        chapters.chunked(limit).flatMap {
-          dao.allForChapters(it)
-        }
+    return appDb.transaction {
+      chapters.runForMaxSqlVariableNumber {
+        dao.allForChapters(it)
       }
-    } else {
-      dao.allForChapters(chapters)
     }
   }
 }
