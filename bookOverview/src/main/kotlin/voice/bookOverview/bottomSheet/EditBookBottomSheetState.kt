265d28e90c0dfd68f5c1295b58875986c42567b1
Shirish Kamath <shirishk.87@gmail.com>
2022-07-15 13:06:37 +0530

    Adding 'Mark as Current', 'Mark as Not Started Yet', 'Mark as Completed'. Fixes #1046 (#1059)

--- bookOverview/src/main/kotlin/voice/bookOverview/bottomSheet/EditBookBottomSheetState.kt
+++ bookOverview/src/main/kotlin/voice/bookOverview/bottomSheet/EditBookBottomSheetState.kt
@@ -2,8 +2,11 @@ package voice.bookOverview.bottomSheet
 
 import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Done
 import androidx.compose.material.icons.outlined.Download
+import androidx.compose.material.icons.outlined.HourglassEmpty
 import androidx.compose.material.icons.outlined.Image
+import androidx.compose.material.icons.outlined.NotStarted
 import androidx.compose.material.icons.outlined.Title
 import androidx.compose.ui.graphics.vector.ImageVector
 import voice.bookOverview.R
@@ -19,4 +22,7 @@ enum class BottomSheetItem(
   Title(R.string.change_book_name, Icons.Outlined.Title),
   InternetCover(R.string.download_book_cover, Icons.Outlined.Download),
   FileCover(R.string.pick_book_cover, Icons.Outlined.Image),
+  BookCategoryMarkAsNotStarted(R.string.mark_as_not_started, Icons.Outlined.HourglassEmpty),
+  BookCategoryMarkAsCurrent(R.string.mark_as_current, Icons.Outlined.NotStarted),
+  BookCategoryMarkAsCompleted(R.string.mark_as_completed, Icons.Outlined.Done),
 }
