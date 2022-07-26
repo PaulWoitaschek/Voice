4f01a10648c14975b449769a76ac6f9a648be979
Paul Woitaschek <woitaschek@gmail.com>
2022-07-26 22:16:58 +0200

    Check if all listing files are included and delete incomplete listings on import.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,35 @@ tasks.wrapper {
 }
 
 tasks {
-  register<Exec>("importStrings") {
+  val pullStrings = register<Exec>("pullStrings") {
     executable = "tx"
-    args("pull", "-a", "--parallel", "--minimum-perc", "1")
+    args("pull", "-a", "--parallel")
+  }
+  val removeEmptyListings = register("removeEmptyListings") {
+    dependsOn(pullStrings)
+    mustRunAfter(pullStrings)
+    val listingsFolder = file("app/src/main/play/listings")
+    doLast {
+      (listingsFolder.listFiles()?.toList() ?: emptyList())
+        .filter { it.isDirectory }
+        .filterNot {
+          // don't remove the source language
+          it.name == "en-US"
+        }
+        .forEach { language ->
+          val contents = language.listFiles()?.toList() ?: emptyList()
+          val requiredListings = listOf("full-description.txt", "short-description.txt", "title.txt")
+            .map { File(language, it) }
+          if (!contents.containsAll(requiredListings)) {
+            logger.warn("Missing translations for ${language.name}. Delete listing.")
+            language.deleteRecursively()
+          }
+        }
+    }
+  }
+  register("importStrings") {
+    dependsOn(pullStrings, removeEmptyListings)
+    mustRunAfter(pullStrings, removeEmptyListings)
     finalizedBy(":app:lintDebug")
   }
 
