ef55c0f973c7094bfaf3f77f9a55e4885eb48070
Paul Woitaschek <woitaschek@gmail.com>
2023-04-04 13:15:42 +0200

    Add a preview to the book overview app bar.

--- bookOverview/src/main/kotlin/voice/bookOverview/views/BookOverviewTopAppBar.kt
+++ bookOverview/src/main/kotlin/voice/bookOverview/views/BookOverviewTopAppBar.kt
@@ -6,11 +6,16 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.collections.immutable.persistentMapOf
+import voice.bookOverview.overview.BookOverviewLayoutMode
 import voice.bookOverview.overview.BookOverviewViewState
 import voice.common.R
+import voice.common.compose.VoiceTheme
 
 @Composable
 internal fun BookOverviewTopAppBar(
@@ -45,4 +50,29 @@ internal fun BookOverviewTopAppBar(
       SettingsIcon(onSettingsClick)
     },
   )
+}
+
+@Composable
+@Preview
+private fun BookOverviewTopAppBarPreview() {
+  VoiceTheme {
+    BookOverviewTopAppBar(
+      scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(),
+      viewState = BookOverviewViewState(
+        books = persistentMapOf(),
+        layoutMode = BookOverviewLayoutMode.List,
+        playButtonState = BookOverviewViewState.PlayButtonState.Paused,
+        showAddBookHint = true,
+        showMigrateHint = true,
+        showMigrateIcon = true,
+        showSearchIcon = true,
+        isLoading = true,
+      ),
+      onSearchClick = {},
+      onBookMigrationClick = {},
+      onBoomMigrationHelperConfirmClick = {},
+      onBookFolderClick = {},
+      onSettingsClick = {},
+    )
+  }
 }
