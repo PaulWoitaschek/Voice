b4069c46450812d25a304e3304d923c518cdb0cb
Paul Woitaschek <woitaschek@gmail.com>
2023-04-04 13:20:44 +0200

    Tint the migrate icon and change it so it's more prominent.

--- bookOverview/src/main/kotlin/voice/bookOverview/views/MigrateIcon.kt
+++ bookOverview/src/main/kotlin/voice/bookOverview/views/MigrateIcon.kt
@@ -2,12 +2,15 @@ package voice.bookOverview.views
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.CompareArrows
+import androidx.compose.material.icons.outlined.Restore
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import voice.common.compose.VoiceTheme
 import voice.strings.R as StringsR
 
 @Composable
@@ -20,12 +23,21 @@ internal fun MigrateIcon(
   Box {
     IconButton(modifier = modifier, onClick = onClick) {
       Icon(
-        imageVector = Icons.Outlined.CompareArrows,
+        imageVector = Icons.Outlined.Restore,
         contentDescription = stringResource(StringsR.string.migration_hint_title),
+        tint = MaterialTheme.colorScheme.primary,
       )
     }
     if (withHint) {
       MigrateHint(onHintClick)
     }
+  }
+}
+
+@Preview
+@Composable
+private fun MigrateIconPreview() {
+  VoiceTheme {
+    MigrateIcon(withHint = false, onClick = {}, onHintClick = {})
   }
 }
