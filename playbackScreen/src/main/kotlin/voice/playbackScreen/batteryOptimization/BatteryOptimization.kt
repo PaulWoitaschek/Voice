5694e5537dc0dc18f7baa3f2f271afa0f2b867be
Paul Woitaschek <woitaschek@gmail.com>
2023-05-07 17:58:44 +0200

    Don't request battery optimizations on google devices.

--- playbackScreen/src/main/kotlin/voice/playbackScreen/batteryOptimization/BatteryOptimization.kt
+++ playbackScreen/src/main/kotlin/voice/playbackScreen/batteryOptimization/BatteryOptimization.kt
@@ -1,5 +1,6 @@
 package voice.playbackScreen.batteryOptimization
 
+import android.os.Build
 import androidx.datastore.core.DataStore
 import kotlinx.coroutines.flow.first
 import javax.inject.Inject
@@ -10,10 +11,10 @@ class BatteryOptimization
   private val amountOfBatteryOptimizationsRequested: DataStore<Int>,
 ) {
 
-  suspend fun shouldRequest(): Boolean = if (isIgnoringBatteryOptimizations()) {
-    false
-  } else {
-    amountOfBatteryOptimizationsRequested.data.first() <= 3
+  suspend fun shouldRequest(): Boolean = when {
+    isIgnoringBatteryOptimizations() -> false
+    Build.MANUFACTURER.equals("Google", ignoreCase = true) -> false
+    else -> amountOfBatteryOptimizationsRequested.data.first() <= 3
   }
 
   suspend fun onBatteryOptimizationsRequested() {
