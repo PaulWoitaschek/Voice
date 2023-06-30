625e59e90512d96f81dda3427b564cf33da38015
Paul Woitaschek <woitaschek@gmail.com>
2023-06-30 23:25:06 +0200

    Stop the service in onTaskRemoved
    If the player isn't set to play when ready, the service is stopped and resources released.
     This is done because if the app is swiped away from recent apps without this check, the notification would remain in an unresponsive state.
    Further explanation can be found at: https://github.com/androidx/media/issues/167#issuecomment-1615184728

--- playback/src/main/kotlin/voice/playback/session/PlaybackService.kt
+++ playback/src/main/kotlin/voice/playback/session/PlaybackService.kt
@@ -1,5 +1,6 @@
 package voice.playback.session
 
+import android.content.Intent
 import androidx.media3.session.MediaLibraryService
 import androidx.media3.session.MediaSession
 import kotlinx.coroutines.CoroutineScope
@@ -32,11 +33,27 @@ class PlaybackService : MediaLibraryService() {
     setMediaNotificationProvider(voiceNotificationProvider)
   }
 
-  override fun onDestroy() {
-    super.onDestroy()
+  override fun onTaskRemoved(rootIntent: Intent?) {
+    super.onTaskRemoved(rootIntent)
+    if (!player.playWhenReady) {
+      // If the player isn't set to play when ready, the service is stopped and resources released.
+      // This is done because if the app is swiped away from recent apps without this check,
+      // the notification would remain in an unresponsive state.
+      // Further explanation can be found at: https://github.com/androidx/media/issues/167#issuecomment-1615184728
+      release()
+      stopSelf()
+    }
+  }
+
+  private fun release() {
     player.release()
     session.release()
     scope.cancel()
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    release()
   }
 
   override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaLibrarySession = session
