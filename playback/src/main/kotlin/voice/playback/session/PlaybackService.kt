ba1115ac78944b8562b080870aa845b17f71cdd9
Paul Woitaschek <woitaschek@gmail.com>
2023-06-30 23:27:24 +0200

    Return null for released sessions.

--- playback/src/main/kotlin/voice/playback/session/PlaybackService.kt
+++ playback/src/main/kotlin/voice/playback/session/PlaybackService.kt
@@ -6,6 +6,7 @@ import androidx.media3.session.MediaSession
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.cancel
 import voice.common.rootComponentAs
+import voice.logging.core.Logger
 import voice.playback.di.PlaybackComponent
 import voice.playback.player.VoicePlayer
 import javax.inject.Inject
@@ -56,5 +57,25 @@ class PlaybackService : MediaLibraryService() {
     release()
   }
 
-  override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaLibrarySession = session
+  override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaLibrarySession? {
+    return session.takeUnless { session ->
+      session.invokeIsReleased
+    }.also {
+      if (it == null) {
+        Logger.e("onGetSession returns null because the session is already released")
+      }
+    }
+  }
 }
+
+private val MediaSession.invokeIsReleased: Boolean
+  get() = try {
+    // temporarily checked to debug
+    // https://github.com/androidx/media/issues/422
+    MediaSession::class.java.getDeclaredMethod("isReleased")
+      .apply { isAccessible = true }
+      .invoke(this) as Boolean
+  } catch (e: Exception) {
+    Logger.e(e, "Couldn't check if it's released")
+    false
+  }
