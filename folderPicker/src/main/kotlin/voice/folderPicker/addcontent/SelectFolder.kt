2abab2a0af198977ab20464274c99bda4f4f742c
Paul Woitaschek <woitaschek@gmail.com>
2023-07-02 08:35:52 +0200

    Ensure the onboarding layouts are all uniform. Make use of weights.
    Fixes #2007

--- folderPicker/src/main/kotlin/voice/folderPicker/addcontent/SelectFolder.kt
+++ folderPicker/src/main/kotlin/voice/folderPicker/addcontent/SelectFolder.kt
@@ -37,37 +37,41 @@ internal fun SelectFolder(
     },
     content = { contentPadding ->
       Column(Modifier.padding(contentPadding)) {
-        if (LocalConfiguration.current.screenHeightDp > 500) {
-          Spacer(modifier = Modifier.size(16.dp))
-          Image(
-            modifier = Modifier
-              .heightIn(max = 300.dp)
-              .padding(horizontal = 32.dp)
-              .align(Alignment.CenterHorizontally),
-            painter = painterResource(id = R.drawable.folder_type_artwork),
-            contentDescription = null,
-          )
+        if (LocalConfiguration.current.screenHeightDp > 600) {
+          Column(Modifier.weight(1F)) {
+            Image(
+              modifier = Modifier
+                .heightIn(max = 400.dp)
+                .padding(top = 32.dp, bottom = 24.dp, start = 24.dp, end = 24.dp)
+                .align(Alignment.CenterHorizontally),
+              painter = painterResource(id = R.drawable.folder_type_artwork),
+              contentDescription = null,
+            )
+          }
         }
-        Spacer(modifier = Modifier.size(16.dp))
 
-        Text(
-          modifier = Modifier.padding(horizontal = 24.dp),
-          text = stringResource(
-            when (mode) {
-              Destination.AddContent.Mode.Default -> StringsR.string.select_folder_title_default
-              Destination.AddContent.Mode.Onboarding -> StringsR.string.select_folder_title_onboarding
-            },
-          ),
-          style = MaterialTheme.typography.displayMedium,
-        )
-        Spacer(modifier = Modifier.size(4.dp))
-        Text(
-          modifier = Modifier.padding(horizontal = 24.dp),
-          text = stringResource(StringsR.string.select_folder_subtitle),
-          style = MaterialTheme.typography.bodyLarge,
-        )
-        Spacer(modifier = Modifier.size(24.dp))
-        SelectFolderButtonRow(onAdd)
+        Column(Modifier.weight(2F)) {
+          Spacer(modifier = Modifier.size(16.dp))
+
+          Text(
+            modifier = Modifier.padding(horizontal = 24.dp),
+            text = stringResource(
+              when (mode) {
+                Destination.AddContent.Mode.Default -> StringsR.string.select_folder_title_default
+                Destination.AddContent.Mode.Onboarding -> StringsR.string.select_folder_title_onboarding
+              },
+            ),
+            style = MaterialTheme.typography.displayMedium,
+          )
+          Spacer(modifier = Modifier.size(4.dp))
+          Text(
+            modifier = Modifier.padding(horizontal = 24.dp),
+            text = stringResource(StringsR.string.select_folder_subtitle),
+            style = MaterialTheme.typography.bodyLarge,
+          )
+          Spacer(modifier = Modifier.size(24.dp))
+          SelectFolderButtonRow(onAdd)
+        }
       }
     },
   )
