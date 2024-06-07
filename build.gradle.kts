13fe025363c8c87b065d915fdcfcc50171978979
Paul Woitaschek <woitaschek@gmail.com>
2024-06-07 19:45:03 +0200

    Adjust for kotlin 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
   alias(libs.plugins.kotlin.jvm) apply false
   alias(libs.plugins.android.app) apply false
   alias(libs.plugins.android.library) apply false
+  alias(libs.plugins.compose.compiler) apply false
   id("voice.ktlint")
 }
 
