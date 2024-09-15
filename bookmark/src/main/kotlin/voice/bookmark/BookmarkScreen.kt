8a007919a0ac9c0f2a89e3e0e2d93980d9f6375c
Paul Woitaschek <woitaschek@gmail.com>
2024-09-15 10:29:41 +0200

    Fix the bottom padding.

--- bookmark/src/main/kotlin/voice/bookmark/BookmarkScreen.kt
+++ bookmark/src/main/kotlin/voice/bookmark/BookmarkScreen.kt
@@ -8,9 +8,8 @@ import androidx.compose.foundation.gestures.DraggableAnchors
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.anchoredDraggable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -39,7 +38,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
@@ -136,11 +134,7 @@ internal fun BookmarkScreen(
     }
     LazyColumn(
       state = lazyListState,
-      modifier = Modifier
-        .padding(paddingValues)
-        .fillMaxSize()
-        .padding(bottom = 88.dp)
-        .clipToBounds(),
+      contentPadding = paddingValues,
     ) {
       items(
         items = viewState.bookmarks,
@@ -153,6 +147,9 @@ internal fun BookmarkScreen(
           onEdit = onEdit,
           onClick = onClick,
         )
+      }
+      item {
+        Spacer(Modifier.size(88.dp))
       }
     }
   }
