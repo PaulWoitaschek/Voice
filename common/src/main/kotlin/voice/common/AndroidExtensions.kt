53de9c4396d68843a867f38127598a3a3e6aa332
Paul Woitaschek <woitaschek@gmail.com>
2024-12-27 19:07:35 +0100

    Remove the unused checkMainThread

--- common/src/main/kotlin/voice/common/AndroidExtensions.kt
+++ common/src/main/kotlin/voice/common/AndroidExtensions.kt
@@ -3,7 +3,6 @@ package voice.common
 import android.content.Context
 import android.os.Build
 import android.os.Bundle
-import android.os.Looper
 import android.os.Parcelable
 import android.util.TypedValue
 import android.view.LayoutInflater
@@ -14,12 +13,6 @@ fun Context.layoutInflater(): LayoutInflater = LayoutInflater.from(this)
 fun Context.dpToPx(dp: Float) = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, resources.displayMetrics)
 
 fun Context.dpToPxRounded(dp: Float): Int = dpToPx(dp).roundToInt()
-
-fun checkMainThread() {
-  check(Looper.getMainLooper() == Looper.myLooper()) {
-    "Is not on ui thread!"
-  }
-}
 
 inline fun <reified T : Parcelable> Bundle.parcelable(key: String): T? {
   return if (Build.VERSION.SDK_INT >= 33) {
