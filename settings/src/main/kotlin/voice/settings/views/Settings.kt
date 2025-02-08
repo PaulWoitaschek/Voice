cd76dd3c92c57e2cc406f318325e64c963479027
Till Davin <till.davin@gmx.de>
2025-02-08 18:45:01 +0100

    feat: update auto sleep timer UI layout

--- settings/src/main/kotlin/voice/settings/views/Settings.kt
+++ settings/src/main/kotlin/voice/settings/views/Settings.kt
@@ -1,9 +1,14 @@
 package voice.settings.views
 
+import AutoSleepTimerSetting
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -17,19 +22,14 @@ import androidx.compose.material.icons.outlined.Lightbulb
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
-import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TimePickerState
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,7 +42,6 @@ import voice.common.rootComponentAs
 import voice.settings.SettingsListener
 import voice.settings.SettingsViewModel
 import voice.settings.SettingsViewState
-import java.time.LocalTime
 import voice.strings.R as StringsR
 
 @Composable
@@ -149,78 +148,33 @@ private fun Settings(
         AutoRewindRow(viewState.autoRewindInSeconds) {
           listener.onAutoRewindRowClick()
         }
+        AutoSleepTimerRow(
+          viewState.autoSleepTimer,
+          listener::toggleAutoSleepTimer,
+        )
         ListItem(
-          colors = ListItemDefaults.colors(containerColor = Color.Transparent),
+          leadingContent = { Spacer(Modifier.size(24.dp)) },
           headlineContent = {
-            Text(text = stringResource(id = StringsR.string.auto_sleep_timer))
-          },
-          trailingContent = {
-            Switch(
-              checked = viewState.autoSleepTimer,
-              onCheckedChange = listener::toggleAutoSleepTimer,
-            )
+            Row(
+              horizontalArrangement = Arrangement.spacedBy(10.dp),
+              verticalAlignment = Alignment.CenterVertically,
+            ) {
+              AutoSleepTimerSetting(
+                viewState.autoSleepTimer,
+                viewState.autoSleepTimeStart,
+                stringResource(id = StringsR.string.auto_sleep_timer_start),
+                listener::setAutoSleepTimerStart,
+              )
+              Spacer(Modifier.weight(weight = 1f, fill = true))
+              AutoSleepTimerSetting(
+                viewState.autoSleepTimer,
+                viewState.autoSleepTimeEnd,
+                stringResource(id = StringsR.string.auto_sleep_timer_end),
+                listener::setAutoSleepTimerEnd,
+              )
+            }
           },
         )
-        if (viewState.autoSleepTimer) {
-          val shouldShowStartTimePicker = remember { mutableStateOf(false) }
-          ListItem(
-            modifier = Modifier.padding(horizontal = 15.dp),
-            colors = ListItemDefaults.colors(containerColor = Color.Transparent),
-            headlineContent = {
-              Text(stringResource(id = StringsR.string.auto_sleep_timer_start))
-            },
-            trailingContent = {
-              if (shouldShowStartTimePicker.value) {
-                val initialTime = LocalTime.parse(viewState.autoSleepTimeStart)
-                TimePickerDialog(
-                  initialTime.hour,
-                  initialTime.minute,
-                  { timePickerState: TimePickerState ->
-                    listener.setAutoSleepTimerStart(timePickerState.hour, timePickerState.minute)
-                    shouldShowStartTimePicker.value = false
-                  },
-                  { shouldShowStartTimePicker.value = false },
-                )
-              }
-              TextButton(
-                onClick = {
-                  shouldShowStartTimePicker.value = true
-                },
-              ) {
-                Text(viewState.autoSleepTimeStart)
-              }
-            },
-          )
-          val shouldShowEndTimePicker = remember { mutableStateOf(false) }
-          ListItem(
-            modifier = Modifier.padding(horizontal = 15.dp),
-            colors = ListItemDefaults.colors(containerColor = Color.Transparent),
-            headlineContent = {
-              Text(stringResource(id = StringsR.string.auto_sleep_timer_end))
-            },
-            trailingContent = {
-              if (shouldShowEndTimePicker.value) {
-                val initialTime = LocalTime.parse(viewState.autoSleepTimeEnd)
-                TimePickerDialog(
-                  initialTime.hour,
-                  initialTime.minute,
-                  { timePickerState: TimePickerState ->
-                    listener.setAutoSleepTimerEnd(timePickerState.hour, timePickerState.minute)
-                    shouldShowEndTimePicker.value = false
-                  },
-                  { shouldShowEndTimePicker.value = false },
-                )
-              }
-              TextButton(
-                onClick = {
-                  shouldShowEndTimePicker.value = true
-                },
-              ) {
-                Text(viewState.autoSleepTimeEnd)
-              }
-            },
-          )
-        }
         ListItem(
           modifier = Modifier.clickable { listener.suggestIdea() },
           leadingContent = { Icon(Icons.Outlined.Lightbulb, stringResource(StringsR.string.pref_suggest_idea)) },
