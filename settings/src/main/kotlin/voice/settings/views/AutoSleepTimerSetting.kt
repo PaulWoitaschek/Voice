e816f7a44f3c9f763b5b4010b40d08b0a01e6bc7
Till Davin <till.davin@gmx.de>
2025-03-02 14:34:41 +0100

    refactor: follow AppVersion composable for disabled color

--- settings/src/main/kotlin/voice/settings/views/AutoSleepTimerSetting.kt
+++ settings/src/main/kotlin/voice/settings/views/AutoSleepTimerSetting.kt
@@ -1,10 +1,9 @@
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.LocalContentColor
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TimePickerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import voice.settings.views.TimePickerDialog
@@ -17,32 +16,31 @@ internal fun AutoSleepTimerSetting(
   label: String,
   setAutoSleepTime: (Int, Int) -> Unit,
 ) {
-  val shouldShowTimePicker = remember { mutableStateOf(false) }
-  CompositionLocalProvider(
-    LocalContentColor provides
-      if (autoSleepTimer) MaterialTheme.colorScheme.onSurface
-      else MaterialTheme.colorScheme.onSurface.copy(alpha = 0.38f),
-  ) {
-    Text(text = label)
-  }
-  if (shouldShowTimePicker.value) {
-    val initialTime = LocalTime.parse(time)
-    TimePickerDialog(
-      initialTime.hour,
-      initialTime.minute,
-      { timePickerState: TimePickerState ->
-        setAutoSleepTime(timePickerState.hour, timePickerState.minute)
-        shouldShowTimePicker.value = false
-      },
-      { shouldShowTimePicker.value = false },
+  Row {
+    val shouldShowTimePicker = remember { mutableStateOf(false) }
+    Text(
+      text = label,
+      color = LocalContentColor.current.copy(alpha = if (autoSleepTimer) 1.0F else 0.5F),
     )
-  }
-  TextButton(
-    onClick = {
-      shouldShowTimePicker.value = true
-    },
-    enabled = autoSleepTimer,
-  ) {
-    Text(text = time)
+    if (shouldShowTimePicker.value) {
+      val initialTime = LocalTime.parse(time)
+      TimePickerDialog(
+        initialTime.hour,
+        initialTime.minute,
+        { timePickerState: TimePickerState ->
+          setAutoSleepTime(timePickerState.hour, timePickerState.minute)
+          shouldShowTimePicker.value = false
+        },
+        { shouldShowTimePicker.value = false },
+      )
+    }
+    TextButton(
+      onClick = {
+        shouldShowTimePicker.value = true
+      },
+      enabled = autoSleepTimer,
+    ) {
+      Text(text = time)
+    }
   }
 }
