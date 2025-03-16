7ff680062b2144dc3006108e2ff8b4663d70cc77
Paul Woitaschek <woitaschek@gmail.com>
2025-03-16 20:43:56 +0100

    Remove the onTaskRemoved workaround as it's now implemented upstream
    See https://github.com/androidx/media/commit/617f9898c3f23608f644ab2c345f017305199c55

--- playback/src/main/kotlin/voice/playback/session/PlaybackService.kt
+++ playback/src/main/kotlin/voice/playback/session/PlaybackService.kt
@@ -1,6 +1,5 @@
 package voice.playback.session
 
-import android.content.Intent
 import androidx.media3.session.MediaLibraryService
 import androidx.media3.session.MediaSession
 import kotlinx.coroutines.CoroutineScope
@@ -32,18 +31,6 @@ class PlaybackService : MediaLibraryService() {
       .create(this)
       .inject(this)
     setMediaNotificationProvider(voiceNotificationProvider)
-  }
-
-  override fun onTaskRemoved(rootIntent: Intent?) {
-    super.onTaskRemoved(rootIntent)
-    if (!player.playWhenReady) {
-      // If the player isn't set to play when ready, the service is stopped and resources released.
-      // This is done because if the app is swiped away from recent apps without this check,
-      // the notification would remain in an unresponsive state.
-      // Further explanation can be found at: https://github.com/androidx/media/issues/167#issuecomment-1615184728
-      release()
-      stopSelf()
-    }
   }
 
   private fun release() {
