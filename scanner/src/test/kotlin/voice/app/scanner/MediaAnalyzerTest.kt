9008b8a884798fb78ae94ebb95309e19fda8c15b
Paul Woitaschek <woitaschek@gmail.com>
2025-05-07 12:14:49 +0200

    Make use of the auphonic test files.

--- scanner/src/test/kotlin/voice/app/scanner/MediaAnalyzerTest.kt
+++ scanner/src/test/kotlin/voice/app/scanner/MediaAnalyzerTest.kt
@@ -3,10 +3,11 @@ package voice.app.scanner
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import io.kotest.assertions.assertSoftly
+import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.longs.shouldBeWithinPercentageOf
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
-import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.runBlocking
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TemporaryFolder
@@ -17,7 +18,6 @@ import voice.documentfile.FileBasedDocumentFile
 import voice.logging.core.LogWriter
 import voice.logging.core.Logger
 import java.io.File
-import kotlin.time.Duration.Companion.seconds
 
 @RunWith(AndroidJUnit4::class)
 @Config(sdk = [33])
@@ -28,75 +28,91 @@ internal class MediaAnalyzerTest {
   val tempFolder = TemporaryFolder()
 
   private val analyzer = MediaAnalyzer(ApplicationProvider.getApplicationContext())
-
-  @Test
-  fun mp3() = test(
-    fileName = "test.mp3",
-    durationAssert = {
-      it.shouldBeWithinPercentageOf(30.seconds.inWholeMilliseconds, 0.2)
-    },
+  private val auphonicChapters = listOf(
+    MarkData(startMs = 0L, name = "Intro"),
+    MarkData(startMs = 15000L, name = "Creating a new production"),
+    MarkData(startMs = 22000L, name = "Sound analysis"),
+    MarkData(startMs = 34000L, name = "Adaptive leveler"),
+    MarkData(startMs = 45000L, name = "Global loudness normalization"),
+    MarkData(startMs = 60000L, name = "Audio restoration algorithms"),
+    MarkData(startMs = 76000L, name = "Output file formats"),
+    MarkData(startMs = 94000L, name = "External services"),
+    MarkData(startMs = 111500L, name = "Get a free account!"),
   )
 
-  @Test
-  fun ogg() = test(fileName = "test.ogg")
+  private fun parse(filename: String): Metadata? {
+    Logger.install(
+      object : LogWriter {
+        override fun log(
+          severity: Logger.Severity,
+          message: String,
+          throwable: Throwable?,
+        ) {
+          println("$severity: $message, $throwable")
+        }
+      },
+    )
+    val testFile = File(javaClass.classLoader!!.getResource(filename)!!.file)
+    val documentFile = FileBasedDocumentFile(testFile)
+    return runBlocking { analyzer.analyze(documentFile) }
+  }
 
   @Test
-  fun opus() = test(
-    fileName = "test.opus",
-    durationAssert = {
-      it.shouldBeWithinPercentageOf(30.seconds.inWholeMilliseconds, 0.2)
-    },
-  )
+  fun mp3() {
+    val metadata = parse("auphonic_chapters_demo.mp3")
+
+    assertSoftly {
+      metadata.shouldNotBeNull()
+      metadata.duration.shouldBeWithinPercentageOf(119040L, 0.2)
+      metadata.fileName shouldBe "auphonic_chapters_demo"
+      metadata.title shouldBe "Auphonic Chapter Marks Demo"
+      metadata.artist shouldBe "Auphonic"
+      metadata.album shouldBe "Auphonic Examples"
+      metadata.chapters shouldContainExactly auphonicChapters
+    }
+  }
 
   @Test
-  fun mka() = test(fileName = "test.mka")
+  fun ogg() {
+    val metadata = parse("auphonic_chapters_demo.ogg")
 
-  private fun test(
-    fileName: String,
-    durationAssert: (Long) -> Unit = {
-      it shouldBe 30.seconds.inWholeMilliseconds
-    },
-  ) {
-    runTest {
-      Logger.install(
-        object : LogWriter {
-          override fun log(
-            severity: Logger.Severity,
-            message: String,
-            throwable: Throwable?,
-          ) {
-            println("$severity: $message, $throwable")
-          }
-        },
-      )
-      val testFile = File(javaClass.classLoader!!.getResource(fileName)!!.file)
-      val documentFile = FileBasedDocumentFile(testFile)
-      val metadata = analyzer.analyze(documentFile)
+    assertSoftly {
+      metadata.shouldNotBeNull()
+      metadata.duration.shouldBeWithinPercentageOf(119040L, 0.2)
+      metadata.fileName shouldBe "auphonic_chapters_demo"
+      metadata.title shouldBe "Auphonic Chapter Marks Demo"
+      metadata.artist shouldBe "Auphonic"
+      metadata.album shouldBe "Auphonic Examples"
+      metadata.chapters shouldContainExactly auphonicChapters
+    }
+  }
 
-      assertSoftly {
-        metadata.shouldNotBeNull()
-        durationAssert(metadata.duration)
-        metadata.chapters.shouldBe(
-          listOf(
-            MarkData(
-              startMs = 0L,
-              name = "Introduction",
-            ),
-            MarkData(
-              startMs = 10000,
-              name = "Chapter 1",
-            ),
-            MarkData(
-              startMs = 20000,
-              name = "Chapter 2",
-            ),
-          ),
-        )
-        metadata.title shouldBe "Test Audiobook"
-        metadata.artist shouldBe "Jane Doe"
-        metadata.fileName shouldBe "test"
-        metadata.album shouldBe "Sample Audio"
-      }
+  @Test
+  fun opus() {
+    val metadata = parse("auphonic_chapters_demo.opus")
+
+    assertSoftly {
+      metadata.shouldNotBeNull()
+      metadata.duration.shouldBeWithinPercentageOf(119040L, 0.2)
+      metadata.fileName shouldBe "auphonic_chapters_demo"
+      metadata.title shouldBe "Auphonic Chapter Marks Demo"
+      metadata.artist shouldBe "Auphonic"
+      metadata.album shouldBe "Auphonic Examples"
+      metadata.chapters shouldContainExactly auphonicChapters
+    }
+  }
+
+  @Test
+  fun m4a() {
+    val metadata = parse("auphonic_chapters_demo.m4a")
+
+    assertSoftly {
+      metadata.shouldNotBeNull()
+      metadata.duration.shouldBeWithinPercentageOf(119040L, 0.2)
+      metadata.fileName shouldBe "auphonic_chapters_demo"
+      metadata.title shouldBe "Auphonic Chapter Marks Demo"
+      metadata.artist shouldBe "Auphonic"
+      metadata.album shouldBe "Auphonic Examples"
     }
   }
 }
