860d76eec300ca4ab22d9fc952d57160a3d0efec
Paul Woitaschek <woitaschek@gmail.com>
2025-05-23 16:01:03 +0200

    Read an audio file in the integration test

--- app/src/androidTest/kotlin/voice/app/SleepTimerIntegrationTest.kt
+++ app/src/androidTest/kotlin/voice/app/SleepTimerIntegrationTest.kt
@@ -1,7 +1,11 @@
 package voice.app
 
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Test
 import voice.app.injection.appComponent
+import java.io.File
 
 class SleepTimerIntegrationTest {
 
@@ -10,5 +14,14 @@ class SleepTimerIntegrationTest {
     val testComponent = appComponent as TestComponent
     val playerController = testComponent.playerController
     playerController.play()
+
+    val context = ApplicationProvider.getApplicationContext<Context>()
+    val outputFile = File(context.filesDir, "auphonic_chapters_demo.m4a")
+    InstrumentationRegistry.getInstrumentation().context.assets
+      .open("auphonic_chapters_demo.m4a").use { inputStream ->
+        outputFile.outputStream().use { outputStream ->
+          inputStream.copyTo(outputStream)
+        }
+      }
   }
 }
