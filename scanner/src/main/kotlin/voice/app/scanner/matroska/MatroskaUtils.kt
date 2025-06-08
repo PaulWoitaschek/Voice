99403977e5b4ab63a08dbdbd69612269a382e0e3
Paul Woitaschek <woitaschek@gmail.com>
2025-06-08 21:23:51 +0200

    Refactor Matroska metadata extraction: move header validation and child element reading to utility functions

---
+++ scanner/src/main/kotlin/voice/app/scanner/matroska/MatroskaUtils.kt
@@ -0,0 +1,56 @@
+package voice.app.scanner.matroska
+
+import org.ebml.EBMLReader
+import org.ebml.Element
+import org.ebml.MasterElement
+import org.ebml.ProtoType
+import org.ebml.StringElement
+import org.ebml.UnsignedIntegerElement
+import org.ebml.io.DataSource
+import org.ebml.matroska.MatroskaDocTypes
+
+infix fun <T : Element> Element?.isType(t: ProtoType<T>) = this != null && isType(t.type)
+
+inline fun Element.forEachChild(dataSource: DataSource, reader: EBMLReader, action: (Element) -> Unit) {
+  if (this !is MasterElement) {
+    throw MatroskaParseException("Expected a MasterElement")
+  }
+  var child = readNextChild(reader)
+  while (child != null) {
+    action(child)
+    child.skipData(dataSource)
+    child = readNextChild(reader)
+  }
+}
+
+fun validateHeader(dataSource: DataSource, reader: EBMLReader) {
+  val header = reader.readNextElement()
+  if (!(header isType MatroskaDocTypes.EBML)) {
+    throw MatroskaParseException("Invalid EBML header")
+  }
+
+  header.forEachChild(dataSource, reader) { element ->
+    if (element isType MatroskaDocTypes.DocType) {
+      val docType = element.readString(dataSource)
+      if (docType !in listOf("matroska", "webm")) {
+        throw MatroskaParseException("Unsupported doc type: $docType")
+      }
+    }
+  }
+}
+
+fun Element.readString(dataSource: DataSource): String {
+  if (this !is StringElement) {
+    throw MatroskaParseException("Expected a StringElement")
+  }
+  readData(dataSource)
+  return value
+}
+
+fun Element.readUnsignedInteger(dataSource: DataSource): Long {
+  if (this !is UnsignedIntegerElement) {
+    throw MatroskaParseException("Expected an UnsignedIntegerElement")
+  }
+  readData(dataSource)
+  return value
+}
