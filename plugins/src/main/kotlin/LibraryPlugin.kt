0300f9b27900dead35356e7dd28bf3b8fe186ee6
Paul Woitaschek <woitaschek@gmail.com>
2025-07-03 22:34:23 +0200

    Generate the robolectric.properties to downgrade the robolectric sdk to a supported version.

--- plugins/src/main/kotlin/LibraryPlugin.kt
+++ plugins/src/main/kotlin/LibraryPlugin.kt
@@ -1,5 +1,9 @@
+import com.android.build.api.dsl.LibraryExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.configure
+import java.io.File
 
 class LibraryPlugin : Plugin<Project> {
 
@@ -12,6 +16,26 @@ class LibraryPlugin : Plugin<Project> {
         target.baseSetup()
         target.tasks.register("voiceUnitTest") {
           dependsOn("testDebugUnitTest")
+        }
+        configureRobolectricSdk(target)
+      }
+    }
+  }
+
+  // there is no global way to set the Robolectric SDK version,
+  // so we generate a file in the test resources
+  private fun configureRobolectricSdk(target: Project) {
+    val libs = target.extensions.getByType(VersionCatalogsExtension::class.java).named("libs")
+    val generatedResourceDir = target.file("build/generated/testResources")
+      .apply { mkdirs() }
+    File(generatedResourceDir, "robolectric.properties").apply {
+      val sdkVersion = libs.findVersion("sdk-robolectric").get().requiredVersion.toInt()
+      writeText("sdk=$sdkVersion")
+    }
+    target.extensions.configure<LibraryExtension> {
+      sourceSets {
+        named("test") {
+          resources.srcDir(generatedResourceDir)
         }
       }
     }
