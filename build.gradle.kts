524336ebd4089d63eca2f1b714aebb4e7ac3b6c6
Paul Woitaschek <woitaschek@posteo.de>
2025-08-23 21:58:37 +0200

    Configure the build-health plugin (#3017)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,4 @@
 plugins {
-  alias(libs.plugins.kotlin.android) apply false
-  alias(libs.plugins.kotlin.jvm) apply false
-  alias(libs.plugins.android.app) apply false
-  alias(libs.plugins.android.library) apply false
   alias(libs.plugins.compose.compiler) apply false
   id("voice.ktlint")
 }
