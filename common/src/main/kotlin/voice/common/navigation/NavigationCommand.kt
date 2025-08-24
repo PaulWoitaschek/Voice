272dfab182b19e0c9249aabbf7e804f9128f7b46
Paul Woitaschek <woitaschek@gmail.com>
2025-08-24 14:05:04 +0200

    Move the Destination into it's own file

--- common/src/main/kotlin/voice/common/navigation/NavigationCommand.kt
+++ common/src/main/kotlin/voice/common/navigation/NavigationCommand.kt
@@ -1,12 +1,5 @@
 package voice.common.navigation
 
-import android.content.Intent
-import android.net.Uri
-import androidx.navigation3.runtime.NavKey
-import kotlinx.serialization.Serializable
-import voice.common.BookId
-import voice.common.serialization.UriSerializer
-
 sealed interface NavigationCommand {
   data object GoBack : NavigationCommand
   data class GoTo(val destination: Destination) : NavigationCommand
@@ -14,70 +7,3 @@ sealed interface NavigationCommand {
   data class SetRoot(val root: Destination.Compose) : NavigationCommand
 }
 
-sealed interface Destination {
-
-  @Serializable
-  data class Playback(val bookId: BookId) : Compose
-  data class Bookmarks(val bookId: BookId) : Compose
-
-  @Serializable
-  data class CoverFromInternet(val bookId: BookId) : Compose
-  data class Website(val url: String) : Destination
-  data class EditCover(
-    val bookId: BookId,
-    val cover: Uri,
-  ) : Compose
-
-  data class Activity(val intent: Intent) : Destination
-
-  @Serializable
-  sealed interface Compose :
-    Destination,
-    NavKey
-
-  @Serializable
-  data object Migration : Compose
-
-  @Serializable
-  data object Settings : Compose
-
-  @Serializable
-  data object BookOverview : Compose
-
-  @Serializable
-  data object FolderPicker : Compose
-
-  @Serializable
-  data class SelectFolderType(
-    val uri:
-    @Serializable(with = UriSerializer::class)
-    Uri,
-    val mode: Mode,
-  ) : Compose {
-
-    enum class Mode {
-      Default,
-      Onboarding,
-    }
-  }
-
-  @Serializable
-  data object OnboardingWelcome : Compose
-
-  @Serializable
-  data object OnboardingCompletion : Compose
-
-  @Serializable
-  data object OnboardingExplanation : Compose
-
-  data object BatteryOptimization : Destination
-
-  @Serializable
-  data class AddContent(val mode: Mode) : Compose {
-
-    enum class Mode {
-      Default,
-      Onboarding,
-    }
-  }
-}
