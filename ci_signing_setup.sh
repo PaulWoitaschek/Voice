c16957124eefba58be79ff143b62e700e2ea9ef5
Paul Woitaschek <woitaschek@gmail.com>
2025-09-04 07:40:21 +0200

    Let the base64 decoding fail fast.

--- ci_signing_setup.sh
+++ ci_signing_setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
+set -euo pipefail
+
 decode_env_to_file() {
   local env_var="${1}"
   local dest_file="${2}"
-  if [[ -n "${!env_var}" ]]; then
-    if [[ ! -f "${dest_file}" ]]; then
-      echo "${!env_var}" | base64 --decode >"${dest_file}"
-      echo "Success: Written to ${dest_file}"
-    else
-      echo "Warning: File ${dest_file} already exists, not overwritten."
-    fi
-  else
-    echo "Warning: Environment variable ${env_var} is empty or not set, no action taken."
+
+  if [[ -z "${!env_var:-}" ]]; then
+    echo "Error: Environment variable ${env_var} is not set or empty." >&2
+    exit 1
   fi
+
+  echo "${!env_var}" | base64 --decode >"${dest_file}"
+  echo "Success: Written to ${dest_file}"
 }
 
 mkdir -p signing/play
