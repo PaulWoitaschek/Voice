719db23404b0deff6582edc3d91537a860f2dbb4
sandreas <2050604+sandreas@users.noreply.github.com>
2025-09-21 18:12:04 +0200

    removed unused code, added logger output for search query

--- core/search/src/main/kotlin/voice/core/search/BookSearch.kt
+++ core/search/src/main/kotlin/voice/core/search/BookSearch.kt
@@ -4,41 +4,25 @@ import dev.zacsweers.metro.Inject
 import voice.core.data.Book
 import voice.core.data.repo.BookRepository
 import voice.core.data.repo.internals.dao.BookContentDao
+import voice.core.logging.api.Logger
 
 @Inject
 class BookSearch(
   private val dao: BookContentDao,
   private val repo: BookRepository,
 ) {
-
-  suspend fun search2(query: String): List<Book> {
-    val searchString = buildString {
+  suspend fun search(query: String): List<Book> {
+    val matchQuery = buildString {
       append(
         query.trim()
           // replace all special chars except umlauts and other language specific letters
           .replace("[^\\p{L}0-9\\s]".toRegex(), " ")
           // replace all whitespaces with *
-          .split("\\s+".toRegex()).joinToString("*"),
+          .split("\\s+".toRegex()).filter { it.isNotEmpty() }.joinToString("*"),
       )
       append('*')
     }
-    val results = dao.search(searchString)
-
-    return results.mapNotNull { repo.get(it) }
-  }
-
-  suspend fun search(query: String): List<Book> {
-    return dao.search(
-      buildString {
-        append(
-          query.trim()
-            // replace all special chars except umlauts and other language specific letters
-            .replace("[^\\p{L}0-9\\s]".toRegex(), " ")
-            // replace all whitespaces with *
-            .split("\\s+".toRegex()).joinToString("*"),
-        )
-        append('*')
-      },
-    ).mapNotNull { repo.get(it) }
+    Logger.d("search with MATCH: $matchQuery")
+    return dao.search(matchQuery).mapNotNull { repo.get(it) }
   }
 }
