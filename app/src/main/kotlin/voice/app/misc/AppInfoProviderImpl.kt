3728e6c9dbda2d84ce2a77535d18a6a5f62495b6
Paul Woitaschek <woitaschek@gmail.com>
2025-10-19 20:04:35 +0200

    Add a consent screen for analytics.

--- app/src/main/kotlin/voice/app/misc/AppInfoProviderImpl.kt
+++ app/src/main/kotlin/voice/app/misc/AppInfoProviderImpl.kt
@@ -7,4 +7,5 @@ import voice.core.common.AppInfoProvider
 @Inject
 class AppInfoProviderImpl : AppInfoProvider {
   override val versionName: String = BuildConfig.VERSION_NAME
+  override val analyticsIncluded: Boolean = BuildConfig.INCLUDE_ANALYTICS
 }
