827247aac366c77c97bd2b1fdefae994aacaa8f6
Paul Woitaschek <woitaschek@gmail.com>
2025-10-25 20:46:21 +0200

    Mock analytics

--- core/playback/src/test/kotlin/voice/core/playback/player/VoicePlayerTest.kt
+++ core/playback/src/test/kotlin/voice/core/playback/player/VoicePlayerTest.kt
@@ -109,6 +109,7 @@ class VoicePlayerTest {
     mediaItemProvider = mediaItemProvider,
     volumeGain = mockk(relaxed = true),
     sleepTimer = mockk(relaxed = true),
+    analytics = mockk(relaxed = true),
   )
 
   @Test
