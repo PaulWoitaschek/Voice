34985ea974a442e54183d543eb918db128f8750b
Paul Woitaschek <woitaschek@gmail.com>
2026-04-18 22:37:43 +0200

    Also disable foojay if COPILOT_AGENT_FIREWALL_ENABLE_RULESET_ALLOW_LIST is set

--- plugins/settings.gradle.kts
+++ plugins/settings.gradle.kts
@@ -5,7 +5,7 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 rootProject.name = "plugins"
 
 plugins {
-  if(System.getenv("CI") != "true") {
+  if(System.getenv("CI") != "true" || System.getenv("COPILOT_AGENT_FIREWALL_ENABLE_RULESET_ALLOW_LIST")!= null) {
     id("org.gradle.toolchains.foojay-resolver-convention") version ("1.0.0")
   }
 }
