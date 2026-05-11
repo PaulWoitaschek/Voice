f7744f19ed8a3971cff17a7fecfd86c943be2f3d
ikanakova <38765251+ikanakova@users.noreply.github.com>
2026-05-11 20:03:27 +0200

    Add padding to AppBarTitle text
    - Added `.padding(end = 16.dp)` to create space on the right edge

--- features/playbackScreen/src/main/kotlin/voice/features/playbackScreen/view/AppBarTitle.kt
+++ features/playbackScreen/src/main/kotlin/voice/features/playbackScreen/view/AppBarTitle.kt
@@ -2,18 +2,21 @@ package voice.features.playbackScreen.view
 
 import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.unit.dp
 
 @Composable
 internal fun AppBarTitle(title: String) {
   Text(
     text = title,
     modifier = Modifier
-      .fillMaxWidth()
-      .clipToBounds()
-      .basicMarquee(),
+    .fillMaxWidth()
+    .padding(end = 16.dp)
+    .clipToBounds()
+    .basicMarquee(),
   )
 }
